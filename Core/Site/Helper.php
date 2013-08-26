df3b35a6ba90cbb74d22b80705bff03993cbc253
Paul Nikitin <paulnikitin@gmail.com>
2013-08-26 15:45:12 +0400

    Передача телефона в переменные шаблона

--- Core/Site/Helper.php
+++ Core/Site/Helper.php
@@ -2,6 +2,7 @@
 namespace Ideal\Core\Site;
 
 use Ideal\Structure\Part\Widget\MainMenu;
+use Ideal\Core\Config;
 
 class Helper
 {
@@ -9,8 +10,14 @@ class Helper
 
     public function getVariables($model)
     {
+        // Получаем данные из виджета главного меню
         $mainMenu = new MainMenu($model);
         $vars['mainMenu'] = $mainMenu->getData();
+
+        // Получаем телефон из конфига site_data.php
+        $config = Config::getInstance();
+        $vars['phone'] = $config->phone;
+
         return $vars;
     }
 
