c1af916086f973a4611a49c81766988dda259554
Олег Парамонов <zeroicce@gmail.com>
2013-09-26 12:09:53 +0400

    Доделал модуль редиректа до рабочего состояния. Для функционирования на сайтах где уже была произведена установка CMS нужно добавить в .htaccess #START redirect #END redirect

--- Structure/Service/Redirect/Action.php
+++ Structure/Service/Redirect/Action.php
@@ -1,3 +1,24 @@
+<?php
+$config = \Ideal\Core\Config::getInstance();
+$file = new \Ideal\Structure\Service\Redirect\RewriteRile();
+
+$file->loadFile($config->cmsFolder . '/redirect.txt');
+
+#$t = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/.htaccess');
+#$file = 'h=h=h='."\n";
+#$file = preg_replace('/(#START redirect)(\r\n|\r|\n)([^}]*)(#END redirect)/', "$1$2".$file."$2$4", $t);
+#file_put_contents($_SERVER['DOCUMENT_ROOT'].'/.htaccess', $file);
+if (isset($_POST['edit'])) {
+    $file->addLine();
+    $file->saveFile();
+    exit;
+}
+if (isset($_POST['delete'])) {
+    $file->deleteLine();
+    $file->saveFile();
+    exit;
+}
+?>
 <table id="redirect" class="table table-hover table-striped">
     <tr>
         <th style="width: 249px">Откуда</th>
@@ -5,15 +26,6 @@
         <th style="text-align: right"></th>
     </tr>
     <?php
-    $config = \Ideal\Core\Config::getInstance();
-    $file = new \Ideal\Structure\Service\Redirect\RewriteRile();
-
-    $file->loadFile($config->cmsFolder . '/redirect.txt');
-
-    if (isset($_POST['edit'])) {
-        $file->saveFile();
-    }
-
     echo $file->showEdit();
     ?>
 </table>
@@ -23,25 +35,40 @@
     Добавить редирект
 </button>
 
+<style>
+    tr:hover .editGroup {
+        display: inline;
+    }
+
+    tr {
+        height: 42px;
+    }
+
+    tr td:last-child {
+        text-align: right;
+        vertical-align: middle;
+    }
+
+    .editLine td {
+        padding-bottom: 0;
+    }
+</style>
+
 
 <script>
-    $('tr').on('onfocus','.editGroup', function(){
-        alert('1');
-        $(this).show();
-        console.log(this);
-    })
     function addLine(e) {
         $(e).attr("disabled", "disabled");
-        var i = parseInt($(e).val());
-        var from = $('[name=from' + (i - 1) + ']').val();
-        var on = $('[name=on' + (i - 1) + ']').val();
+        var i = parseInt($(e).val()) + 1;
+        var from = $('#line' + (i - 1) + ' > .from').children().val();
+        var on = $('#line' + (i - 1) + ' > .on').children().val();
         if (from !== '' && on !== '') {
-            $('#redirect > tbody:last').append('<tr onfocus="aloha()" style="height: 41px" id="line' + i + '">'
-                + '<td class="from"></td><td class="on"></td>'
-                + '<td style="text-align: right;"><div class="hide editGroup"> '
+            $('#redirect > tbody:last').append('<tr id="line' + i + '">'
+                + '<td class="from"><input type="text" name="from"></td>'
+                + '<td class="on"><input type="text" name="on"></td>'
+                + '<td><div class="hide editGroup"> '
                 + '<span class="input-prepend">'
-                + '<button style="width: 47px;" onclick="editLine(' + i + ')" title="Изменить" class="btn btn-info btn-mini">'
-                + '<i class="icon-pencil icon-white"></i></button></span>'
+                + '<button type="button" style="width: 47px;" onclick="saveLine(' + i + ')" title="Сохранить" class="btn btn-success btn-mini">'
+                + '<i class="icon-ok icon-white"></i></button></span>'
                 + '<span class="input-append"><button onclick="delLine(' + i + ')" title="Удалить" class="btn btn-danger btn-mini">'
                 + '<i class="icon-remove icon-white"></i></button></span></div>'
                 + '</td></tr>')
@@ -49,50 +76,100 @@
         }
         $(e).removeAttr('disabled');
     }
-    function aloha(){
-        alert('aloha');
-    }
 
     function delLine(e) {
+        var line = $('#line' + e);
+        var from = line.find('.from');
+        var on = line.find('.on');
+        var fromVal = from.children().val();
+        if(fromVal === undefined){
+            fromVal = from.html();
+        }
+        var onVal = on.children().val();
+        if(onVal === undefined){
+            onVal = on.html();
+        }
+
+        if (fromVal === '' && onVal === '') {
+            $('#line' + e).remove();
+            return true;
+        }
+        if (!confirm('Удалить?')) {
+            return false;
+        }
+        $.ajax({
+            type: "POST",
+            data: "delete=1&from=" + fromVal + "&on=" + onVal
+        });
         $('#line' + e).remove();
     }
 
     function editLine(e) {
         var line = $('#line' + e);
+        line.addClass('editLine');
         var butedit = line.find('.btn-info').removeClass('btn-info').addClass('btn-success');
         butedit.attr('onclick', 'saveLine(' + e + ')');
+        butedit.attr('title', 'Сохранить');
         butedit.children().removeClass('icon-pencil').addClass('icon-ok');
+        line.find('.btn-danger').val(line.children('.from').text());
         var from = line.find('.from');
+        from.attr('prevVal', from.html());
         var on = line.find('.on');
+        on.attr('prevVal', on.html());
         from.html('<input type="text" name="from" value="' + from.html() + '">');
         on.html('<input type="text" name="on" value="' + on.html() + '">');
+        console.log(line);
     }
 
-    function saveLine(e){
+    function saveLine(e) {
         var line = $('#line' + e);
-
         var from = line.find('.from');
         var on = line.find('.on');
         var fromVal = from.children().val();
         var onVal = on.children().val();
-        if(onVal == '' || fromVal == ''){
+        if (onVal == '' || fromVal == '') {
             alert('Заполнены не все поля!');
             return false;
         }
-        if(fromVal == onVal){
-            alert('Бесцонечный редирект самого на себя!');
+        if (fromVal == onVal) {
+            alert('Бесконечный редирект самого на себя!');
             return false;
         }
-        $.ajax({
-            type: "POST",
-            data: "edit=1&from=" + fromVal + "&on=" + onVal
-        });
-        from.html(fromVal);
-        on.html(onVal);
+        if (fromVal == from.attr('prevVal') && onVal === on.attr('prevVal')) {
+            line.removeClass('editLine');
+            from.html(fromVal);
+            on.html(onVal);
 
-        var butedit = line.find('.btn-success').removeClass('btn-success').addClass('btn-info');
-        butedit.attr('onclick', 'editLine(' + e + ')');
-        butedit.children().removeClass('icon-ok').addClass('icon-pencil');
+            var butedit = line.find('.btn-success').removeClass('btn-success').addClass('btn-info');
+            butedit.attr('onclick', 'editLine(' + e + ')');
+            butedit.attr('title', 'Изменить');
+            butedit.children().removeClass('icon-ok').addClass('icon-pencil');
+            return true;
+        }
+        $.ajax({
+            dataType: 'json',
+            type: "POST",
+            data: "edit=1&from=" + fromVal + "&on=" + onVal,
+            success: function (data) {
+                if (data.error) {
+                    $('#line' + data.line).css('background', 'lightcyan');
+                    alert(data.text);
+                    return false;
+                } else {
+                    line.removeClass('editLine');
+                    from.html(fromVal);
+                    on.html(onVal);
+
+                    var butedit = line.find('.btn-success').removeClass('btn-success').addClass('btn-info');
+                    butedit.attr('onclick', 'editLine(' + e + ')');
+                    butedit.attr('title', 'Изменить');
+                    butedit.children().removeClass('icon-ok').addClass('icon-pencil');
+                }
+            }
+        });
+    }
+
+    function ckeck(data) {
     }
 
 </script>
--- Structure/Service/Redirect/RewriteRile.php
+++ Structure/Service/Redirect/RewriteRile.php
@@ -1,83 +1,173 @@
 <?php
-namespace Ideal\Structure\Service\Redirect;
 
-use Ideal\Core\Util;
+namespace Ideal\Structure\Service\Redirect;
 
 class RewriteRile
 {
-    protected $fileName;
-    protected $params;
+    protected $fileName; // файл с редиректами
+    protected $params; // правила для редиректов где ключ 1 - откуда, 2 - куда перенаправлять
+    protected $htaccess = true; // Сохранять в htaccess или нет
 
     public function loadFile($fileName)
     {
         $this->params = array();
         $this->fileName = $fileName;
+        if (!file_exists($this->fileName)) {
+            $fp = fopen($this->fileName, "w"); // создаем файл
+            fclose($fp);
+        }
         $file_handle = fopen($this->fileName, "r");
         while (!feof($file_handle)) {
             $line = trim(fgets($file_handle));
             $line = preg_replace("/\s{2,}/", " ", $line);
-            if(strlen($line) < 10) continue;
-            $line = explode(' ',$line, 4);
+            if (strlen($line) < 10)
+                continue;
+            $line = explode(' ', $line, 4);
             $this->params[] = $line;
         }
         fclose($file_handle);
     }
 
-    public function getCountParam(){
+    public function getCountParam()
+    {
         return count($this->params);
     }
 
-
-    public function saveFile()
+    /**
+     * Создает новый редирект
+     * Возвращает текст, для парсера js, ввиде json. Где
+     * error - была ли ошибка
+     * line - номер строки из-за которой появилась ошибка
+     * text - подробное описание
+     */
+    public function addLine()
     {
+        $answer = array('error' => false);
         // Изменяем постоянные настройки сайта
-        $fp = fopen($this->fileName, 'w');
-        $file = '';
         $tmp = array();
         $tmp[0] = 'RewriteRule';
         $tmp[1] = $_POST['from'];
         $tmp[2] = $_POST['on'];
         $tmp[3] = '[R=301,L]';
+        foreach ($this->params as $k => $v) {
+            $k++; // В таблице нумерация строк начинается с 1, а не с 0
+
+            // Проверяем на повторение редиректа, цикличностии и отсутсвие цепочек редиректов
+            if (strnatcasecmp($v[1], $tmp[1]) == 0) {
+                if (strnatcasecmp($v[2], $tmp[2]) == 0) {
+                    $answer['text'] = "Такой уже редирект существует в строке {$k}";
+                } else {
+                    $answer['text'] = "Редирект с {$tmp[1]} уже существует и переадресует на {$tmp[2]}";
+                }
+                $answer['error'] = true;
+            }
+            if (strnatcasecmp($tmp[2], $v[1]) == 0) {
+                if (strnatcasecmp($tmp[1], $v[2]) == 0) {
+                    $answer['text'] = "Организуется бесконечный редирект с {$tmp[1]} на {$tmp[2]}, помогает этому строка {$k}";
+                } else {
+                    $answer['text'] = "Организуется множественный редирект с {$tmp[1]} на {$tmp[2]}, а потом на {$v[2]}";
+                }
+                $answer['error'] = true;
+            }
+            if (strnatcasecmp($tmp[1], $v[2]) == 0) {
+                if (strnatcasecmp($tmp[2], $v[1]) == 0) {
+                    $answer['text'] = "Организуется бесконечный редирект с {$tmp[1]} на {$tmp[2]}, помогает этому строка {$k}";
+                } else {
+                    $answer['text'] = "Организуется множественный редирект с {$v[1]} на {$v[2]}, а потом на {$tmp[2]}";
+                }
+                $answer['error'] = true;
+            }
+            if ($answer['error']) {
+                print json_encode($answer);
+                exit;
+            }
+        }
         $this->params[] = $tmp;
         unset($tmp);
-        foreach($this->params as $k => $v){
-            if(!isset($v[1]) || $v[1] =='') continue;
-            if(!isset($v[2]) || $v[2] =='') continue;
-            if(strnatcasecmp($v[1], $v[2]) === 0) continue;
+        print json_encode($answer);
+    }
+
+    /**
+     * Удаляет один из редиректов
+     */
+    public function deleteLine()
+    {
+        $tmp[1] = $_POST['from'];
+        $tmp[2] = $_POST['on'];
+        foreach ($this->params as $k => $v) {
+            // Ищем нужный нам редирект
+            if (strnatcasecmp($v[1], $tmp[1]) == 0) {
+                if (strnatcasecmp($v[2], $tmp[2]) == 0) {
+                    unset($this->params[$k]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Сохраняет изменения в редиректах в файл
+     * Во время записипроверяет чтобы не было циклов редиректов, пустых редиректов
+     */
+    public function saveFile()
+    {
+        $fp = fopen($this->fileName, 'w');
+        $file = '';
+        foreach ($this->params as $k => $v) {
+            if (!isset($v[1]) || $v[1] == '')
+                continue;
+            if (!isset($v[2]) || $v[2] == '')
+                continue;
+            if (strnatcasecmp($v[1], $v[2]) === 0)
+                continue;
             $file .= "{$v[0]} {$v[1]} {$v[2]} {$v[3]}\n";
         }
         fwrite($fp, $file);
+        // Записсь в htaccess
+        if($this->htaccess){
+            $t = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/.htaccess');
+            $file = preg_replace('/(#START redirect)(\r\n|\r|\n)([^}]*)(#END redirect)/', "$1$2".$file."$2$4", $t);
+            file_put_contents($_SERVER['DOCUMENT_ROOT'].'/.htaccess', $file);
+        }
     }
 
-    public function checkUrl(){
-        $url = $_GET['url'];
-        foreach($this->params as $v){
-            if(strnatcasecmp($v[1],$url)===0){
+    /**
+     * TODO реализиция редиректов посредствам PHP
+     */
+    public function checkUrl($url = null)
+    {
+        $url = ($url == null) ? $_GET['url'] : $url;
+        foreach ($this->params as $v) {
+            if (strnatcasecmp($v[1], $url) === 0) {
                 // TODO перенаправить по редиректу
             }
         }
-
     }
 
-
+    /**
+     * Выводит уже существующие редиректы в виде строки таблицы
+     * @return string
+     */
     public function showEdit()
     {
         $str = '';
-        foreach($this->params as $k => $v){
+        $i = 1;
+        foreach ($this->params as $k => $v) {
             $str .= <<<RULE
-            <tr id="line{$k}">
+            <tr id="line{$i}">
 <td class="from">{$v[1]}</td><td class="on">{$v[2]}</td>
-<td style="text-align: right;">
+<td><div class="hide editGroup">
     <span class="input-prepend">
-    <button style="width: 47px;" onclick="editLine({$k})" title="Изменить" class="btn btn-info btn-mini">
+    <button style="width: 47px;" onclick="editLine({$i})" title="Изменить" class="btn btn-info btn-mini">
     <i class="icon-pencil icon-white"></i></button></span>
-    <span class="input-append"><button onclick="delLine({$k})" title="Удалить" class="btn btn-danger btn-mini">
-    <i class="icon-remove icon-white"></i></button></span>
+    <span class="input-append"><button onclick="delLine({$i})" title="Удалить" class="btn btn-danger btn-mini">
+    <i class="icon-remove icon-white"></i></button></span></div>
 </td>
 </tr>
 RULE;
+            $i++;
         }
         $str .= '';
         return $str;
     }
+
 }
