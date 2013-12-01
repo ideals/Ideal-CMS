67f21793b9d473deb7ca211a172d56b3a838893a
Paul Nikitin <paulnikitin@gmail.com>
2013-12-01 14:22:45 +0400

    FIX: упрощена обработка сообщений об ошибке, сообщения сделаны более информативными

--- Structure/Service/Backup/Action.php
+++ Structure/Service/Backup/Action.php
@@ -1,86 +1,102 @@
-<form class="form-inline">
-    <button type="submit" name="createMysqlDump" id="createMysqlDump" onclick="createDump(); return false;" class="btn btn-primary">Создать backup базы</button>
-    <span id='textDumpStatus' style="padding-left: 10px;"></span>
-</form>
-
 <?php
 use Ideal\Core\Config;
 $config = Config::getInstance();
 
-// Название дирректории
-$tmpDir = $config->tmpDir;
-$backupDir = $config->tmpDir . '/backup/';
+try {
+    // Пытаемся определить полный путь к папке бэкапов, если это возможно
+    $backupPart = getDir($config->tmpDir, '/backup/');
+} catch (Exception $e) {
+    echo '<div class="alert">' . $e->getMessage() . '</div>';
+    return;
+}
+?>
 
-// Полный путь к дирректории
-$tmpPart = $tmpDir;
-$backupPart = $backupDir;
+<form class="form-inline">
+    <button type="submit" name="createMysqlDump" id="createMysqlDump" onclick="createDump(); return false;"
+            class="btn btn-primary pull-right">
+        Создать backup базы
+    </button>
+    <span id='textDumpStatus'></span>
+</form>
 
-// Сообщение об ошибке
-// Получение полных путей для временной папки и папки для бэкапа
-$error = getDir($tmpPart, $backupPart);
+<div style="clear:right;"></div>
 
-if (!is_null($error)){
-    echo "<script>
-            $('#textDumpStatus').removeClass().addClass('text-error').html('" . $error . "');
-            $('#createMysqlDump').hide();
-        </script>";
-} else {
-    echo '<p>' . 'Раздел для сохранения дампов базы данных: &nbsp;' . $backupPart . '</p>';
-    // Получение списка файлов
-    $dumpFiles = array();
-    if ($error == '') {
-        if ($dh = opendir($backupPart)) {
-            echo '<table id="dumpTable" class="table table-hover">';
-            while (($file = readdir($dh)) !== false) {
-                if (strripos($file, 'dump_') === false) continue;
-                //$dumpFiles[] = $file;
-                //$fn = str_replace('dump_', '',$file);
-                $year = substr($file,5,4);
-                $month = substr($file,10,2);
-                $day = substr($file,13,2);
-                $hour = substr($file,16,2);
-                $minute = substr($file,19,2);
-                $second = substr($file,22,2);
+<?php
+echo '<p>Папка с архивами: &nbsp;' . $backupPart . '</p>';
+// Получение списка файлов
+$dumpFiles = array();
+if ($error == '') {
+    if ($dh = opendir($backupPart)) {
+        echo '<table id="dumpTable" class="table table-hover">';
+        while (($file = readdir($dh)) !== false) {
+            if (strripos($file, 'dump_') === false) continue;
+            //$dumpFiles[] = $file;
+            //$fn = str_replace('dump_', '',$file);
+            $year = substr($file,5,4);
+            $month = substr($file,10,2);
+            $day = substr($file,13,2);
+            $hour = substr($file,16,2);
+            $minute = substr($file,19,2);
+            $second = substr($file,22,2);
 
-                echo '<tr id="' . $file . '"><td><a href="' . $backupDir . $file . '"> ' . "$day/$month/$year - $hour:$minute:$second" . '</a></td>';
-                echo '<td><button class="btn btn-danger btn-mini" title="Удалить" onclick="delDump(\'' . $file . '\'); false;"> <i class="icon-remove icon-white"></i> </button></td>';
-                echo '</tr>';
-            }
-            closedir($dh);
-            echo '</table>';
+            echo '<tr id="' . $file . '"><td><a href="' . $backupPart . $file . '"> ';
+            echo "$day/$month/$year - $hour:$minute:$second" . '</a></td>';
+            echo '<td>';
+            echo '<button class="btn btn-danger btn-mini" title="Удалить" onclick="delDump(\'' . $file . '\'); false;">';
+            echo ' <i class="icon-remove icon-white"></i> ';
+            echo '</button></td>';
+            echo '</tr>';
         }
+        closedir($dh);
+        echo '</table>';
     }
 }
 
 
-// Проверяем временую папку и папку для бэкапов на существование, возможность создания и записи
-// Получаем текст ошибки
-// Задаём полные пути для временной папки и папки для бэкапа
-function getDir(&$tmpPart, &$backupPart) {
-    $tmpDir = $tmpPart;
-    $backupDir = $backupPart;
-    // Временная папка
-    $tmpPart = stream_resolve_include_path($tmpDir);
-    // Проверяем существует ли временная папка и можноли её создать
-    if ($tmpPart === false) {
-        if (mkdir($tmpDir, 0755)) {
-            $tmpPart = stream_resolve_include_path($tmpDir);
-        }
-    }
-    if ($tmpPart === false) return "Не удалось создать папку $tmpDir для сохранения дампа базы";
-    if (!is_writable($tmpDir)) return "Папка $tmpDir недоступна для записи";
 
-    // Проверяем существует ли папка для создания бэкапов
-    // если нет, то пытаемся её создать
-    $backupPart = stream_resolve_include_path($backupDir);
-    if ($backupPart === false) {
-        if (mkdir($backupDir, 0755)) {
-            $backupPart = stream_resolve_include_path($backupDir);
+/**
+ * Функция получения полного пути к папке бэкапа
+ * Проверяем временную папку и папку для бэкапов на существование, возможность создания и записи
+ * @param string $tmpDir Временная папка
+ * @param string $backupDir Название папки бэкапа относительно временной папки
+ * @return string Полный путь к папке бэкапа
+ * @throws Exception Исключение, содержащее текст возникшей ошибки
+ */
+function getDir($tmpDir, $backupDir)
+{
+    if ($tmpDir == '') {
+        throw new Exception('Не задана временная папка tmpDir в файле site_data.php');
+    }
+
+    // Определяем доступность временной папки
+    $tmpDir = DOCUMENT_ROOT . $tmpDir;
+    $tmpFull = stream_resolve_include_path($tmpDir);
+
+    // Проверяем существует ли временная папка и если нет, то пытаемся её создать
+    if ($tmpFull === false) {
+        if (mkdir($tmpDir, 0755)) {
+            $tmpFull = stream_resolve_include_path($tmpDir);
         }
     }
-    if ($backupPart === false) return "Не удалось создать папку $tmpDir для сохранения дампа базы";
-    if (!is_writable($backupPart)) return "Папка $tmpDir недоступна для записи";
-    return null;
+
+    if ($tmpFull === false) throw new Exception("Не удалось создать папку $tmpDir для сохранения дампа базы");
+
+    if (!is_writable($tmpFull)) throw new Exception("Папка $tmpFull недоступна для записи");
+
+    // Проверяем существует ли папка для создания бэкапов и если нет, то пытаемся её создать
+    $backupDir = $tmpFull . $backupDir;
+    $backupFull = stream_resolve_include_path($backupDir);
+    if ($backupFull === false) {
+        if (mkdir($backupDir, 0755)) {
+            $backupFull = stream_resolve_include_path($backupDir);
+        }
+    }
+
+    if ($backupFull === false) throw new Exception("Не удалось создать папку $backupDir для сохранения дампа базы");
+
+    if (!is_writable($backupFull)) throw new Exception("Папка $backupFull недоступна для записи");
+
+    return $backupFull;
 }
 
 ?>
@@ -104,13 +120,13 @@ function delDump(idFile) {
                 if (message == true) {
                     var el = document.getElementById(idFile);
                     el.parentNode.removeChild(el);
-                    $('#textDumpStatus').removeClass().addClass('text-success').html('Файл успешно удалён');
+                    $('#textDumpStatus').removeClass().addClass('alert alert-success').html('Файл успешно удалён');
                 } else {
-                    $('#textDumpStatus').removeClass().addClass('text-error').html('Ошибка при удалении файла');
+                    $('#textDumpStatus').removeClass().addClass('alert alert-error').html('Ошибка при удалении файла');
                 }
             },
             error: function() {
-                $('#textDumpStatus').removeClass().addClass('text-error').html('Не удалось удалить файл');
+                $('#textDumpStatus').removeClass().addClass('alert alert-error').html('Не удалось удалить файл');
             }
         })
     } else {
@@ -120,27 +136,27 @@ function delDump(idFile) {
 
 // Создание дампа базы данных
 function createDump() {
-    $('#textDumpStatus').removeClass().addClass('text-info').html('Идёт создание дампа базы');
+    $('#textDumpStatus').removeClass().addClass('alert alert-info').html('Идёт создание дампа базы');
     var path = window.location.href;
     $.ajax({
         url: path + "&action=ajaxCreateDump",
         type: 'POST',
         data: {
             createMysqlDump: true,
-            backupPart: '<?php echo $backupDir?>'
+            backupPart: '<?php echo addslashes($backupPart)?>'
         },
         success: function(data){
             //Выводим сообщение
             var message = data;
             if (message.length > 1) {
-                $('#textDumpStatus').removeClass().addClass('text-success').html('Дамп базы создн');
+                $('#textDumpStatus').removeClass().addClass('alert alert-success').html('Дамп базы создан');
                 $('#dumpTable').append(data);
             } else {
-                $('#textDumpStatus').removeClass().addClass('text-error').html('Ошибка при создании дампа базы');
+                $('#textDumpStatus').removeClass().addClass('alert alert-error').html('Ошибка при создании дампа базы');
             }
         },
         error: function() {
-            $('#textDumpStatus').removeClass().addClass('text-error').html('Не удалось создать дамп базы');
+            $('#textDumpStatus').removeClass().addClass('alert alert-error').html('Не удалось создать дамп базы');
         }
     })
 }
