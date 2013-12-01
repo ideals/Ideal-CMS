f2aea38192d0c77da0e735b135c0e4c5e3918d44
Paul Nikitin <paulnikitin@gmail.com>
2013-12-01 20:44:24 +0400

    ADD: скачивание бэкапов с помощью ajax-запроса Возможность скачивать архивы вне корня веб-сервера, для скачивания требуется авторизация в админке

--- Structure/Service/Backup/Action.php
+++ Structure/Service/Backup/Action.php
@@ -25,31 +25,31 @@ try {
 echo '<p>Папка с архивами: &nbsp;' . $backupPart . '</p>';
 // Получение списка файлов
 $dumpFiles = array();
-if ($error == '') {
-    if ($dh = opendir($backupPart)) {
-        echo '<table id="dumpTable" class="table table-hover">';
-        while (($file = readdir($dh)) !== false) {
-            if (strripos($file, 'dump_') === false) continue;
-            //$dumpFiles[] = $file;
-            //$fn = str_replace('dump_', '',$file);
-            $year = substr($file,5,4);
-            $month = substr($file,10,2);
-            $day = substr($file,13,2);
-            $hour = substr($file,16,2);
-            $minute = substr($file,19,2);
-            $second = substr($file,22,2);
 
-            echo '<tr id="' . $file . '"><td><a href="' . $backupPart . $file . '"> ';
-            echo "$day/$month/$year - $hour:$minute:$second" . '</a></td>';
-            echo '<td>';
-            echo '<button class="btn btn-danger btn-mini" title="Удалить" onclick="delDump(\'' . $file . '\'); false;">';
-            echo ' <i class="icon-remove icon-white"></i> ';
-            echo '</button></td>';
-            echo '</tr>';
-        }
-        closedir($dh);
-        echo '</table>';
+if ($dh = opendir($backupPart)) {
+    echo '<table id="dumpTable" class="table table-hover">';
+    while (($file = readdir($dh)) !== false) {
+        if (strripos($file, 'dump_') === false) continue;
+        //$dumpFiles[] = $file;
+        //$fn = str_replace('dump_', '',$file);
+        $year = substr($file,5,4);
+        $month = substr($file,10,2);
+        $day = substr($file,13,2);
+        $hour = substr($file,16,2);
+        $minute = substr($file,19,2);
+        $second = substr($file,22,2);
+
+        echo '<tr id="' . $file . '"><td>';
+        echo '<a href="" onClick="return downloadDump(\'' . addslashes($backupPart . '/' . $file) . '\')"> ';
+        echo "$day/$month/$year - $hour:$minute:$second" . '</a></td>';
+        echo '<td>';
+        echo '<button class="btn btn-danger btn-mini" title="Удалить" onclick="delDump(\'' . $file . '\'); false;">';
+        echo ' <i class="icon-remove icon-white"></i> ';
+        echo '</button></td>';
+        echo '</tr>';
     }
+    closedir($dh);
+    echo '</table>';
 }
 
 
@@ -101,8 +101,8 @@ function getDir($tmpDir, $backupDir)
 
 ?>
 
-<script>
-dir = '<?php echo $backupDir ?>';
+<script type="text/javascript">
+
 // Удаление файла
 function delDump(idFile) {
     var nameFile = dir + idFile;
@@ -159,5 +159,25 @@ function createDump() {
             $('#textDumpStatus').removeClass().addClass('alert alert-error').html('Не удалось создать дамп базы');
         }
     })
+}
+
+function downloadDump(data)
+{
+    var url = window.location.href;
+    data = window.location.search.substr(1).split('?') + '&file=' + data + "&action=ajaxDownload";
+    method = 'get';
+
+    // Разрезаем параметры в input'ы
+    var inputs = '';
+    jQuery.each(data.split('&'), function(){
+        var pair = this.split('=');
+        inputs += '<input type="hidden" name="'+ pair[0] +'" value="'+ pair[1] +'" />';
+    });
+
+    // Отправляем запрос
+    jQuery('<form action="'+ url +'" method="'+ (method||'post') +'">'+inputs+'</form>')
+        .appendTo('body').submit().remove();
+
+    return false;
 }
 </script>
---
+++ Structure/Service/Backup/ajaxDownloadAction.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Скачивание файла
+ */
+header('Content-Description: File Transfer');
+header('Content-Type: application/force-download');
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Pragma: public');
+
+if (!isset($_GET['file'])) {
+    header('Content-Disposition: attachment; filename=file-name-not-set');
+    header('Content-Length: 0');
+    exit;
+}
+
+if (!is_file($_GET['file'])) {
+    header('Content-Disposition: attachment; filename=NOT-FOUND-' . basename($_GET['file']));
+    header('Content-Length: 0');
+    exit;
+}
+
+header('Content-Disposition: attachment; filename=' . basename($_GET['file']));
+header('Content-Length: ' . filesize($_GET['file']));
+
+ob_clean();
+flush();
+readfile($result);
+
+exit;
