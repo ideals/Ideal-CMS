15e1e1ac569a2459861675f1dd71002170db9a74
Help2_Neox <help2@neox.ru>
2013-12-23 15:32:04 +0400

    Завершена работа над системой обновления CMS: Service/UpdateCms

--- Structure/Service/UpdateCms/Action.php
+++ Structure/Service/UpdateCms/Action.php
@@ -1,41 +1,90 @@
+<?php
+/**
+ * Сервис обновления IdealCMS
+ * Должен присутствовать на каждом сайте, отвечает за представление информации об обновлении
+ */
+
+use Ideal\Core\Config;
+
+?>
+
 <p id="message">
     Внимание! Обновление в рамках одинакового первого номера происходит автоматически.<br />
     Обновление на другой первый номер версии требует ручного вмешательства.<br />
     <hr />
 </p>
-
-<div id = "form-input">
+<div id="form-input">
 </div>
 
 <?php
 // Сервер обновлений
-$getVersionScript = 'http://idealcms/update/version.php';
+$getVersionScript = 'http://idealcms.ru/update/version.php';
 
 $config = \Ideal\Core\Config::getInstance();
 
 // todo Хранение версий
 
 // Установленные версии CMS и модулей
-$nowVersions = array(
-    'CMS'       => '3.11',
-    'Articles'  => '1.10',
-    'Cabinet'   => '1.10',
-    'Gallery'   => '1.10',
-    'Shop'      => '1.10',
-);
+$nowVersions = getVersions();
 
 $domain = urlencode($config->domain);
+// Сервер обновлений
 $url = $getVersionScript . '?domain=' . $domain . '&ver=' .  urlencode(serialize($nowVersions));
-//Перевожу в формат json для передачи в JS
+// Переводим информацию о версиях в формат json для передачи в JS
 $nowVersions = json_encode($nowVersions);
 
+// Подключаем библиотеку для использования jsonp
+echo '<script type="text/javascript" src="Ideal/Structure/Service/UpdateCms/jquery.jsonp-2.4.0.min.js"> </script>';
 
-echo <<<SCREPT
-    <script type="text/javascript" src="Ideal/Structure/Service/UpdateCms/jquery.jsonp-2.4.0.min.js"> </script>
-SCREPT;
+function getVersions()
+{
+    // Получаем файл README.md для cms
+    $config = Config::getInstance();
+    $mdFile = 'README.md';
+    // Путь к файлу README.md для cms
+    $cmsMdFileName = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/Ideal/' . $mdFile;
+    // Получаем версию cms
+    $versions['Ideal-CMS'] = getVersionFromFile($cmsMdFileName);
+
+    // Ищем файлы README.md в модулях
+    $modDirName = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/Mods';
+    // Получаем версии модулей
+    $modDirs = array_diff(scandir($modDirName), array('.', '..')); // получаем массив папок модулей
+    foreach ($modDirs as $dir) {
+        $modDir  = $modDirName . '/' . $dir  . '/' . $mdFile;
+        $version = getVersionFromFile($modDir); // пытаемся извлечь номер версии из файла README.md
+        if ($version) {
+            $versions[$dir] = $version;
+        }
+    }
+    return $versions;
+}
+
+function getVersionFromFile($cmsMdFileName)
+{
+    if (!file_exists($cmsMdFileName)) return false;
+
+    // Получаем первую строку файла с наименованием и версией
+    $file = fopen($cmsMdFileName, "r");
+    $firstLine = trim(fgets($file));
+    fclose($file);
+
+    // Получаем номер версии из первой строки. Формат номера: пробел+v.+пробел+номер-версии+пробел-или-конец-строки
+    preg_match_all('/\sv\.(\s*)(.*)(\s*)/i', $firstLine, $ver);
+
+    // Если номер версии не удалось определить — выходим
+    if (!isset($ver[2][0]) || ($ver[2][0] == '')) return false;
+
+    return $ver[2][0];
+}
+
 ?>
 
+
 <script type="text/javascript">
+/**
+ * Скрипт получения новых версий и создания представления
+ */
     $.jsonp({
         url: '<?php echo $url ?>',
         callbackParameter: 'callback',
@@ -78,6 +127,10 @@ SCREPT;
         }
     });
 
+
+/*
+ * Скрипт обновления cms и модулей
+ * */
     function updateModule(moduleName, version)
     {
         $.ajax({
@@ -89,7 +142,7 @@ SCREPT;
                 config: '<?php echo $config->cmsFolder; ?>'
             },
             success: function(data){
-                //Выводим сообщение и обновляем страницу
+                // Выводим сообщение и обновляем страницу
                 var message = $.parseJSON(data);
                 alert(message['message']);
                 location.reload();
