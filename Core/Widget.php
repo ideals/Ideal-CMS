1ccbdd4c2f9bb8065f8b7bb492a7ce4ccc3da922
Paul Nikitin <paulnikitin@gmail.com>
2014-02-20 14:50:03 +0400

    ADD: в абстрактный виджет перенесены два наиболее часто используемых в виджетах метода

--- Core/Widget.php
+++ Core/Widget.php
@@ -8,6 +8,12 @@ namespace Ideal\Core;
 
 abstract class Widget
 {
+    /** @var  \Ideal\Core\Site\Model */
+    protected $model;
+    /** @var  string Префикс url для списка ссылок, генерируемых виджетом */
+    protected $prefix;
+    /** @var  string prev_structure для получения элементов в виджете */
+    protected $prevStructure;
 
     public function __construct($model)
     {
@@ -16,4 +22,13 @@ abstract class Widget
 
     abstract public function getData();
 
+    public function setPrevStructure($prevStructure)
+    {
+        $this->prevStructure = $prevStructure;
+    }
+
+    public function setPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+    }
 }
