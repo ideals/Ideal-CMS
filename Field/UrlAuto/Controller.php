e6839821a5aa97713357e4070bc4d51cadc99289
Paul Nikitin <paulnikitin@gmail.com>
2014-02-28 14:21:41 +0400

    FIX: корректная обработка url с завершающим слэшом при редактировании в админке

--- Field/UrlAuto/Controller.php
+++ Field/UrlAuto/Controller.php
@@ -13,6 +13,7 @@ class Controller extends Url\Controller
         $url = new Url\Model();
         $value = array('url' => htmlspecialchars($this->getValue()));
         $link = $url->getUrlWithPrefix($value, $this->model->getParentUrl());
+        $link = $url->cutSuffix($link);
         // Проверяем, является ли url этого объекта частью пути
         $addOn = '';
         if (($link{0} == '/') AND ($value != $link)) {
@@ -27,28 +28,4 @@ class Controller extends Url\Controller
              . '<button id="UrlAuto" type="button" class="btn btn-danger" onclick="javascript:setTranslit(this)">auto url off</button>'
              . '</div>';
     }
-
-
-    public function getValueForList($values, $fieldName)
-    {
-        $url = new Url\Model($fieldName);
-        $link = $url->getUrlWithPrefix($values, $this->model->getParentUrl());
-        if ($link == '---') {
-            // Если это страница внутри главной, то просто возвращаем поле url
-            $link = $values[$fieldName];
-        } else {
-            // Если это не страница внутри Главной, то делаем ссылку
-            $link = '<a href="' . $link . '" target="_blank">' . $link . '</a>';
-        }
-        return $link;
-    }
-
-
-    public function pickupNewValue()
-    {
-        // В url не нужны пробелы ни спереди, ни сзади
-        $value = trim(parent::pickupNewValue());
-        return $value;
-    }
-
 }
