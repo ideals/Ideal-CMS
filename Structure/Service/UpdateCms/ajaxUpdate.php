831e492d88ac131a013cfdb444b2ab5e66cb510c
Евгений Иванов <ivanoveo@mail.ru>
2014-06-24 19:34:36 +0400

    Обновление CMS и модулей. Получение версий текущей CMS  и модулей.

--- Structure/Service/UpdateCms/ajaxUpdate.php
+++ Structure/Service/UpdateCms/ajaxUpdate.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * 1 Получаем версии из update.log, а также, при наличии, названия ранее выполненных файлов
+ * 2 Если не удалось получить доступ к файлу выводим сообщение об ошибке
+ * 4 Выполнение скриптов для текущей версии (оставшихся или всех), записывая при этом каждый успешно выполненный скрипт
+ * 5 Дозапись в update.log новой версии (берётся название следующего раздела с обновлениями)
+ * 6 Если новая версия не соответствует необходимой, возвращаемся к пункту 1
+ * */
+
 /**
  * Сервис обновления IdealCMS
  * Должен присутствовать на каждом сайте, отвечает за получение новых версий cms и модулей,
@@ -44,22 +52,24 @@ $config->loadSettings();
 $srv = 'http://idealcms.ru/update';
 $getFileScript = $srv . '/get.php';
 
+// Файл лога обновлений
+$log = $config->cmsFolder . '/' . 'update.log';
+if (!file_exists($log)) {
+    uExit('Файл лога обновлений не существует ' . $log);
+}
+if (file_put_contents($log, '', FILE_APPEND) === false){
+    uExit('Файл ' . $log . ' недоступен для записи');
+}
 
 if (is_null($config->tmpDir) || ($config->tmpDir == '')) {
-    $message = array(
-        'message ' => 'В настройках не указана папка для хранения временных файлов'
-    );
-    exit(json_encode($message));
+    uExit('В настройках не указана папка для хранения временных файлов');
 }
 
 // Папка для хранения загруженный файлов обновлений
 $uploadDir = DOCUMENT_ROOT . $config->tmpDir . '/update/';
 if (!file_exists($uploadDir)) {
     if (!mkdir($uploadDir, 0755, true)) {
-        $message = array(
-            'message ' => 'Не удалось создать папку' . $uploadDir
-        );
-        exit(json_encode($message));
+        uExit('Не удалось создать папку' . $uploadDir);
     }
 }
 
@@ -68,10 +78,7 @@ if (!file_exists($uploadDir)) {
 define('SETUP_DIR', $uploadDir . '/Setup');
 if (!file_exists(SETUP_DIR)) {
     if (!mkdir(SETUP_DIR, 0755, true)) {
-        $message = array(
-            'message ' => 'Не удалось создать папку' . SETUP_DIR
-        );
-        exit(json_encode($message));
+        uExit('Не удалось создать папку' . SETUP_DIR);
     }
 }
 
@@ -87,10 +94,7 @@ if (isset($_POST['version']) && (isset($_POST['name']))) {
 
     // Проверка получен ли ответ от сервера
     if (strlen($file) === 0) {
-        $message = array(
-            'message' => 'Файл пуст'
-        );
-        exit(json_encode($message));
+        uExit('Файл пуст');
     }
 
     // Если вместо файла найдено сообщение, выводим его
@@ -110,10 +114,7 @@ if (isset($_POST['version']) && (isset($_POST['name']))) {
     }
 
     if (!isset($fileGet['md5'])) {
-        $message = array(
-            'message' => 'Не удалось получить хеш получаемого файла'
-        );
-        exit(json_encode($message));
+        uExit('Не удалось получить хеш получаемого файла');
     }
 
     // Путь к файлу архива
@@ -122,10 +123,7 @@ if (isset($_POST['version']) && (isset($_POST['name']))) {
     file_put_contents($archive, $fileGet['file']);
 
     if (md5_file($archive) != $fileGet['md5']) {
-        $message = array(
-            'message' => 'Полученный файл повреждён (хеш не совпадает)'
-        );
-        exit(json_encode($message));
+        uExit('Полученный файл повреждён (хеш не совпадает)');
     }
 }
 
@@ -141,10 +139,7 @@ if (isset($archive)) {
         unlink($uploadDir . $updateName);
         $archive = true;
     } else {
-        $message = array(
-            'message' => 'Не получилось из-за ошибки #' . $res
-        );
-        exit(json_encode($message));
+        uExit('Не получилось из-за ошибки #' . $res);
     }
 }
 
@@ -160,23 +155,37 @@ if ($archive === true){
     }
     // Переименовывем папку, которую собираемся заменить
     if (!rename($updateCore , $updateCore . '_old')) {
-        $message = array(
-            'message' => 'Не удалось переименовать папку ' . $updateCore
-        );
-        exit(json_encode($message));
+        uExit('Не удалось переименовать папку ' . $updateCore);
     }
     // Перемещаем новую папку на место старой
     if (!rename(SETUP_DIR,  $updateCore)) {
-        $message = array(
-            'message' => 'Не удалось переименовать папку ' . $updateCore
-        );
-        exit(json_encode($message));
+        uExit('Не удалось переименовать папку ' . $updateCore);
     }
+    //Запускаем выполнение скриптов и запросов
+    runProcess();
     // Удаляем старую директорию
     removeDirectory($updateCore . '_old');
 
+    exit('Обновление завершено успешно');
+}
+
+function runProcess()
+{
+
+}
+
+/**
+ * Завершение выполнения скрипта с выводом сообщения
+ *
+ * @param string $msg Сообщение которое нужно передать в качестве результата работы скрипта
+ * @throws Exception если аргумент функции не является строкой
+ */
+function uExit($msg) {
+    if (!is_string($msg)) {
+        throw new Exception("Необходим аргумент типа строка");
+    }
     $message = array(
-        'message' => 'Обновление завершено успешно'
+        'message ' => $msg
     );
     exit(json_encode($message));
 }
