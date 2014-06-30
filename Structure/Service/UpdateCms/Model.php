8332f3f68a811f6165b731422bfed3b471ac750b
Paul Nikitin <paulnikitin@gmail.com>
2014-06-30 11:24:06 +0400

    Перенёс код запуска скриптов в класс модели обновления

--- Structure/Service/UpdateCms/Model.php
+++ Structure/Service/UpdateCms/Model.php
@@ -10,6 +10,7 @@
 namespace Ideal\Structure\Service\UpdateCms;
 
 use Ideal\Core\Config;
+use Ideal\Core\Db;
 
 /**
  * Получение номеров версий установленной CMS и модулей
@@ -19,6 +20,19 @@ class Model
 {
     /** @var string Сообщение об ошибке при попытке получения номеров версий */
     protected $errorText = '';
+    /** @var array Массив папок для обновления */
+    protected $updateFolders = array();
+    /** @var string Путь к файлу с логом обновлений */
+    protected $log = '';
+
+    /**
+     * Инициализация путей к нужным папкам и файлам
+     */
+    public function __construct()
+    {
+        $config = Config::getInstance();
+        $this->log = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . 'update.log';
+    }
 
     /**
      * @return array Массив с номерами установленных версий
@@ -55,9 +69,8 @@ class Model
      */
     protected function getVersionFromFile($mods)
     {
-        $config = Config::getInstance();
         // Файл лога обновлений
-        $log = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . 'update.log';
+        $log = $this->log;
 
         // Проверяем файл update.log
         if (file_put_contents($log, '', FILE_APPEND) === false) {
@@ -115,8 +128,8 @@ class Model
     /**
      * Запись версий в update.log
      *
-     * @param array $version Версии полученные из Readme.ms
-     * @param string $log Файл с логом обновлений
+     * @param array  $version Версии полученные из Readme.ms
+     * @param string $log     Файл с логом обновлений
      */
     protected function putVersionLog($version, $log)
     {
@@ -165,4 +178,247 @@ class Model
     {
         return $this->errorText;
     }
+
+    /**
+     * Завершение выполнения скрипта с выводом сообщения
+     *
+     * @param string $msg Сообщение которое нужно передать в качестве результата работы скрипта
+     * @throws \Exception если аргумент функции не является строкой
+     */
+    public function uExit($msg)
+    {
+        if (!is_string($msg)) {
+            throw new \Exception("Необходим аргумент типа строка");
+        }
+        $message = array(
+            'message' => $msg
+        );
+        exit(json_encode($message));
+    }
+
+    /**
+     * Удаление папки или её очистка
+     *
+     * @param string $dir   Папка которую необходимо удалить или очистить
+     * @param bool   $clear Если значение ложь, то удаляем папку, если истина, очищаем
+     * @return bool
+     */
+    public function removeDirectory($dir, $clear = false)
+    {
+        $res = true;
+        $files = array_diff(scandir($dir), array('.', '..'));
+        foreach ($files as $file) {
+            $res = (is_dir("$dir/$file")) ? $this->removeDirectory("$dir/$file") : unlink("$dir/$file");
+        }
+        if (!$clear) {
+            return rmdir($dir);
+        }
+        return $res;
+    }
+
+    /**
+     * Загрузка и распаковка архива с обновлением модуля
+     *
+     * @param string $updateName Название модуля
+     * @param string $updateVersion Номер версии, на которую обновляемся
+     * @throws \Exception
+     */
+    public function downloadUpdate($updateName, $updateVersion)
+    {
+        $updateUrl = $this->updateFolders['getFileScript'] . '?name=' . urlencode(serialize($updateName))
+            . '&ver=' . $updateVersion;
+        $file = file_get_contents($updateUrl);
+
+        // Проверка получен ли ответ от сервера
+        if (strlen($file) === 0) {
+            $this->uExit('Не удалось получить файл обновления с сервера обновлений');
+        }
+
+        // Если вместо файла найдено сообщение, выводим его
+        $prefix = substr($file, 0, 5);
+        if ($prefix === "(msg)") {
+            $msg = substr($file, 5, strlen($file));
+            $msg = json_decode($msg);
+            if (!isset($msg->message)) {
+                $msg = array(
+                    'message' => "Получен непонятный ответ: " . $file
+                );
+            }
+            exit(json_encode($msg));
+        }
+
+        // Если получили md5
+        if ($prefix !== "(md5)") {
+            $this->uExit("Ответ от сервера некорректен:\n" . $file);
+        }
+
+        $fileGet = array(
+            'md5'  => substr($file, 5, strpos($file, 'md5end') - 5),
+            'file' => substr($file, strpos($file, 'md5end') + 6)
+        );
+
+        if (!isset($fileGet['md5'])) {
+            $this->uExit('Не удалось получить хеш получаемого файла');
+        }
+
+        // Сохраняем полученный архив в свою папку (например, /www/example.com/tmp/update)
+        $archive = $this->updateFolders['uploadDir'] . $updateName;
+        file_put_contents($archive, $fileGet['file']);
+
+        if (md5_file($archive) != $fileGet['md5']) {
+            $this->uExit('Полученный файл повреждён (хеш не совпадает)');
+        }
+
+        // После успешной загрузки архива, распаковываем его
+        $zip = new \ZipArchive;
+        $res = $zip->open($archive);
+
+        if ($res !== true) {
+            $this->uExit('Не получилось из-за ошибки #' . $res);
+        }
+
+        // Очищаем папку перед распаковкой в неё файлов
+        $this->removeDirectory(SETUP_DIR, true);
+
+        // Распаковываем архив в папку
+        $zip->extractTo(SETUP_DIR);
+        $zip->close();
+        unlink($archive);
+
+        // Если разархивирование произошло успешно
+
+        // Определяем путь к тому что мы обновляем, cms или модули
+        $config = Config::getInstance();
+        if ($updateName  == "Ideal-CMS") {
+            // Путь к cms
+            $updateCore = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . "Ideal";
+        } else {
+            // Путь к модулям
+            $updateCore = DOCUMENT_ROOT . '/' . $config->cmsFolder. '/' . "Mods". '/' . $updateName;
+        }
+
+        // Переименовывем папку, которую собираемся заменить
+        if (!rename($updateCore, $updateCore . '_old')) {
+            $this->uExit('Не удалось переименовать папку ' . $updateCore);
+        }
+
+        // Перемещаем новую папку на место старой
+        if (!rename(SETUP_DIR, $updateCore)) {
+            $this->uExit('Не удалось переименовать папку ' . $updateCore);
+        }
+    }
+
+    /**
+     * Запись строки в log-файл
+     * @param string $msg Строка для записи в log
+     */
+    public function writeLog($msg)
+    {
+        $msg = rtrim($msg) . "\n";
+        file_put_contents($this->log, $msg, FILE_APPEND);
+    }
+
+    /**
+     * Установка путей к папкам для обновления модулей
+     *
+     * @param $array
+     */
+    public function setUpdateFolders($array)
+    {
+        $this->updateFolders = array(
+            'getFileScript' => $array['getFileScript'],
+            'uploadDir' => $array['uploadDir']
+        );
+    }
+
+    /**
+     * Запуск скриптов обновления модуля
+     *
+     * @param string $updateName Название модуля
+     * @param string $updateVersion Номер версии, на которую обновляемся
+     */
+    public function updateScripts($updateName, $updateVersion)
+    {
+        // Находим путь к последнему установленному скрипту модуля
+        $logFile = file($this->log);
+        $str = ($updateName == 'Ideal-CMS') ? '/Ideal/Setup/Update' : '/Mods/' . $updateName . '/Setup/Update';
+        $lastScript = '';
+        foreach ($logFile as $v) {
+            if (strpos($v, $str) === 0) {
+                $lastScript = $v;
+            }
+        }
+
+        if ($lastScript != '') {
+            // Находим номер версии и название файла последнего установленного скрипта
+            $scriptArr = explode('/', $lastScript);
+            $scriptEnd = array_pop($scriptArr);
+            $currentVersion = array_pop($scriptArr);
+        } else {
+            $versions = $this->getVersions(); // получаем список установленных модулей
+            $currentVersion = $versions[$updateName];
+        }
+
+        // Считываем названия папок со скриптами обновления
+        $config = Config::getInstance();
+        $updateFolder = DOCUMENT_ROOT . '/' . $config->cmsFolder . $str;
+        $updates = array_diff(scandir($updateFolder), array('.', '..'));
+
+        // Убираем из списка файлы
+        foreach ($updates as $k => $v) {
+            if (!is_dir($updateFolder . '/' . $v)) {
+                unset($updates[$k]);
+            }
+        }
+
+        // Сортируем папки по номерам версий
+        usort($updates, function ($a, $b) {
+            return version_compare($a, $b);
+        });
+
+        // Убираем из списка папки с установленными обновлениями
+        foreach ($updates as $k => $v) {
+            if (version_compare($v, $currentVersion) < 0) {
+                unset($updates[$k]);
+            }
+        }
+
+        // Составление списка скриптов для обновления
+        $scripts = array();
+        foreach ($updates as $folder) {
+            $scriptFolder = $updateFolder . $folder;
+            $files = array_diff(scandir($scriptFolder), array('.', '..'));
+            foreach ($files as $file) {
+                $file = $scriptFolder . '/' . $file;
+                if (is_dir($file)) {
+                    continue;
+                }
+                if ($lastScript == $file) {
+                    // Нашли последний установленный скрипт, значит отсекаем все предыдущие скрипты
+                    $scripts = array();
+                    continue;
+                }
+                $scripts[] = $file;
+            }
+        }
+
+        // Производим запуск скриптов обновления
+        $db = Db::getInstance();
+        foreach ($scripts as $script) {
+            $ext = substr($script, strrpos($script, '.'));
+            switch ($ext) {
+                case '.php':
+                    include $script;
+                    break;
+                case '.sql':
+                    $query = file_get_contents($script);
+                    $db->query($query);
+                    break;
+                default:
+                    continue;
+            };
+            $this->writeLog($script);
+        }
+    }
+
 }
--- Structure/Service/UpdateCms/ajaxUpdate.php
+++ Structure/Service/UpdateCms/ajaxUpdate.php
@@ -7,18 +7,8 @@
  * @license   http://idealcms.ru/license.html LGPL v3
  */
 
-/*
- * 1 Получаем версии из update.log, а также, при наличии, названия ранее выполненных файлов
- * 2 Если не удалось получить доступ к файлу выводим сообщение об ошибке
- * 4 Выполнение скриптов для текущей версии (оставшихся или всех), записывая при этом каждый успешно выполненный скрипт
- * 5 Дозапись в update.log новой версии (берётся название следующего раздела с обновлениями)
- * 6 Если новая версия не соответствует необходимой, возвращаемся к пункту 1
- * */
-
 /**
- * Сервис обновления IdealCMS
- * Должен присутствовать на каждом сайте, отвечает за получение новых версий cms и модулей,
- * их применение
+ * Обновление IdealCMS или одного модуля
  *
  */
 
@@ -56,28 +46,33 @@ $config->errorLog = 'firebug';
 $config->loadSettings();
 
 
+
+$updateModel = new \Ideal\Structure\Service\UpdateCms\Model();
+
 // Сервер обновлений
-$srv = 'http://idealcms.ru/update';
+$srv = 'http://idealcms/update';
 $getFileScript = $srv . '/get.php';
 
 // Файл лога обновлений
-$log = $config->cmsFolder . '/' . 'update.log';
+$log = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . 'update.log';
+
 if (!file_exists($log)) {
-    uExit('Файл лога обновлений не существует ' . $log);
+    $updateModel->uExit('Файл лога обновлений не существует ' . $log);
 }
-if (file_put_contents($log, '', FILE_APPEND) === false){
-    uExit('Файл ' . $log . ' недоступен для записи');
+
+if (file_put_contents($log, '', FILE_APPEND) === false) {
+    $updateModel->uExit('Файл ' . $log . ' недоступен для записи');
 }
 
 if (is_null($config->tmpDir) || ($config->tmpDir == '')) {
-    uExit('В настройках не указана папка для хранения временных файлов');
+    $updateModel->uExit('В настройках не указана папка для хранения временных файлов');
 }
 
-// Папка для хранения загруженный файлов обновлений
+// Папка для хранения загруженных файлов обновлений
 $uploadDir = DOCUMENT_ROOT . $config->tmpDir . '/update/';
 if (!file_exists($uploadDir)) {
     if (!mkdir($uploadDir, 0755, true)) {
-        uExit('Не удалось создать папку' . $uploadDir);
+        $updateModel->uExit('Не удалось создать папку' . $uploadDir);
     }
 }
 
@@ -86,221 +81,33 @@ if (!file_exists($uploadDir)) {
 define('SETUP_DIR', $uploadDir . '/Setup');
 if (!file_exists(SETUP_DIR)) {
     if (!mkdir(SETUP_DIR, 0755, true)) {
-        uExit('Не удалось создать папку' . SETUP_DIR);
+        $updateModel->uExit('Не удалось создать папку' . SETUP_DIR);
     }
 }
 
-
-$dbAdapter = Ideal\Core\Db::getInstance();
-
+$updateModel->setUpdateFolders(array(
+    'getFileScript' => $getFileScript,
+    'uploadDir' => $uploadDir
+));
 
 // Загрузка файла
-if (isset($_POST['version']) && (isset($_POST['name']))) {
-    $updateName  = $_POST['name'];
-    $updateVer  = $_POST['version'];
-    $a =  $getFileScript . '?name=' . urlencode(serialize($updateName )) . '&ver=' . $_POST['version'];
-    $file = file_get_contents($getFileScript . '?name=' . urlencode(serialize($updateName )) . '&ver=' . $_POST['version']);
 
-    // Проверка получен ли ответ от сервера
-    if (strlen($file) === 0) {
-        uExit('Файл пуст');
-    }
+// todo Сделать защиту от хакеров на POST-переменные
 
-    // Если вместо файла найдено сообщение, выводим его
-    $prefix = substr($file, 0, 5);
-    if ($prefix === "(msg)"){
-        $msg = substr($file, 5, strlen($file));
-        $msg = json_decode($msg);
-    }
-    if (isset($msg->message)) {
-        exit(json_encode($msg));
-    }
-
-    // Если получили md5
-    if ($prefix === "(md5)"){
-        $fileGet['md5'] = substr($file, 5, strpos($file, 'md5end') - 5);
-        $fileGet['file'] = substr($file, strpos($file, 'md5end') + 6);
-    }
-
-    if (!isset($fileGet['md5'])) {
-        uExit('Не удалось получить хеш получаемого файла');
-    }
-
-    // Путь к файлу архива
-    // Пример /www/example.com/tmp/update
-    $archive = $uploadDir . $updateName ;
-    file_put_contents($archive, $fileGet['file']);
-
-    if (md5_file($archive) != $fileGet['md5']) {
-        uExit('Полученный файл повреждён (хеш не совпадает)');
-    }
+if (!isset($_POST['version']) || !isset($_POST['name'])) {
+    $updateModel->uExit('Непонятно, что обновлять. Не указаны version и name');
 }
 
-// После успешной загрузки архива, распаковываем его
-if (isset($archive)) {
-    $zip = new ZipArchive;
-    $res = $zip->open($archive);
-    // Очищаем папку перед распаковкой в неё файлов
-    removeDirectory(SETUP_DIR, true);
-    if ($res === true) {
-        $zip->extractTo(SETUP_DIR);
-        $zip->close();
-        unlink($uploadDir . $updateName);
-        $archive = true;
-    } else {
-        uExit('Не получилось из-за ошибки #' . $res);
-    }
-}
+// Скачиваем и распаковываем архив с обновлениями
+$updateModel->downloadUpdate($_POST['name'], $_POST['version']);
 
-// Если разархивирование произошло успешно
-if ($archive === true){
-    // Определяем путь к тому что мы обновляем, cms или модули
-    if ($updateName  == "Ideal-CMS") {
-        // Путь к cms
-        $updateCore = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . "Ideal";
-    } else {
-        // Путь к модулям
-        $updateCore = DOCUMENT_ROOT . '/' . $config->cmsFolder. '/' . "Mods". '/' . $updateName;
-    }
-    // Переименовывем папку, которую собираемся заменить
-    if (!rename($updateCore , $updateCore . '_old')) {
-        uExit('Не удалось переименовать папку ' . $updateCore);
-    }
-    // Перемещаем новую папку на место старой
-    if (!rename(SETUP_DIR,  $updateCore)) {
-        uExit('Не удалось переименовать папку ' . $updateCore);
-    }
-    //Запускаем выполнение скриптов и запросов
-    runProcess($updateCore);
-    // Удаляем старую директорию
-    removeDirectory($updateCore . '_old');
+// Запускаем выполнение скриптов и запросов
+$updateModel->updateScripts($_POST['name'], $_POST['version']);
 
-    exit('Обновление завершено успешно');
-}
+// Модуль установился успешно, делаем запись в лог обновлений
+$this->writeLog('Installed ' . $_POST['name'] . ' v. ' . $_POST['version']);
 
-/**
- * Запуск применения скриптов для определённое версии
- */
-function runProcess($updateCore)
-{
-    global $log, $updateVer, $updateName;
-    $applyScripts = parseUpdateLog($log, $updateName, $updateVer);
+// Удаляем старую папку
+$updateModel->removeDirectory($updateCore . '_old');
 
-    // Выполняем скрипты, получаем следующую версию
-    scriptsExe($updateCore, $updateVer);
-    // Если версия не соответствует необходимой изменяем $updateVer, вызываем runProcess
-}
-
-/**
- * Выполненение скриптов
- */
-function scriptsExe($updateCore, $updateVer, $applyScripts = null)
-{
-    $path = $updateCore . '/' . 'Setup/Update/';
-    //Получение скриптов для модуля или админки
-    $scripts = scandir($path . $updateVer);
-    //Отброс скриптов которые удалось выполнить
-    //Последовательное выполнение скриптов и запросов
-    if (!is_null($applyScripts))
-    foreach($scripts as $k => $v) {
-        if (in_array($v, $applyScripts)) {
-            unset($scripts[$k]);
-        }
-        $type = explode('.', $v);
-        $type = end($type);
-        switch ($type) {
-            case 'php': runPhp($v, $path);
-                break;
-            case 'sql': runSql($v, $path);
-                break;
-        }
-    }
-    //Получение новой версии и её возврат
-    $versions = scandir($updateCore . '/' . 'Setup/Update/');
-    $k = array_search($updateVer, $versions);
-    if ($k === false) return false;
-    $newVer = $versions[$k];
-    return $newVer;
-}
-
-function runPhp($script, $path)
-{
-    exec('php ' . $path . '/' . $script);
-}
-
-function runSql($script, $path)
-{
-    global $dbAdapter;
-    $sql = file_get_contents($path . '/' . $script);
-    $sqlArr = explode(";\n",trim($sql));
-    if (!empty($sqlArr)) {
-        foreach ($sqlArr as $query) {
-            $query = trim($query);
-            if (!empty($query)) {
-                $dbAdapter->query($query);
-            }
-        }
-    }
-}
-
-/**
- * Разбор файла лога обновлений, для получения последней применённой версии модулей и ранее применённых скриптов обновлений
- *
- * @param $log Файл лога обновлений
- * @param $updateName Название модуля
- * @param $updateVer Версия модулей
- *
- * @return array|bool Возвращает true в случае нахождения нужной версии и отсутствия невыполненных скриптов,
- * false, если ненаходит нужную версию модуля, массив невыполненных скриптов
- */
-function parseUpdateLog($log, $updateName, $updateVer)
-{
-    $linesLog = file($log);
-    $vSize = count($linesLog);
-    $applyScripts = array();
-    for($i = $vSize - 1; $i>=0; $i--) {
-        // Удаление спец символов конца строки (необходимость в таком удалении возникает в ОС Windows)
-        $linesLog[$i] = rtrim($linesLog[$i]);
-        if ($linesLog[$i] != '[updateInfo]') continue;
-        if ($linesLog[$i+1] !== ('name=' . $updateName)) continue;
-        if ($linesLog[$i+2] !== ('ver=' . $updateVer)) continue;
-        if ($linesLog[$i+3] == '[updateInfo]') return true;
-        for ($j = $i +3; $j <= $vSize - 1; $j++) {
-            if ($linesLog[$i] == '[updateInfo]') return $applyScripts;
-            $applyScripts = $linesLog[$j];
-        }
-    }
-    return false;
-}
-
-/**
- * Завершение выполнения скрипта с выводом сообщения
- *
- * @param string $msg Сообщение которое нужно передать в качестве результата работы скрипта
- * @throws Exception если аргумент функции не является строкой
- */
-function uExit($msg) {
-    if (!is_string($msg)) {
-        throw new Exception("Необходим аргумент типа строка");
-    }
-    $message = array(
-        'message' => $msg
-    );
-    exit(json_encode($message));
-}
-
-/*
- * Удаление директории или её очистка
- * @param string $dir Папка которую необходимо удалить или очистить
- * @param bool $clear Если значение лож, то удаляем папку, если истина, очищаем
- *  */
-
-function removeDirectory($dir, $clear = false) {
-    $files = array_diff(scandir($dir), array('.','..'));
-    foreach ($files as $file) {
-        (is_dir("$dir/$file")) ? removeDirectory("$dir/$file") : unlink("$dir/$file");
-    }
-    if (!$clear) {
-        return rmdir($dir);
-    }
-}
+$updateModel->uExit('Обновление завершено успешно');
