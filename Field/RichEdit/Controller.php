7632b05e1b919505ff0d91d00fded62a0576bca2
Paul Nikitin <paulnikitin@gmail.com>
2014-07-23 15:12:11 +0400

    При обычном подключении RichEdit появляются ВСЕ кнопки

--- Field/RichEdit/Controller.php
+++ Field/RichEdit/Controller.php
@@ -39,30 +39,9 @@ class Controller extends AbstractController
                 id="{$this->htmlName}">{$value}</textarea>
             <script>
                 CKFinder.setupCKEditor( null, "/{$config->cmsFolder}/Ideal/Library/ckfinder/" );
-                CKEDITOR.replace("{$this->htmlName}", {
-                toolbar: [
-                    [ "Source", "-", "Preview", "Print", "-", "Templates" ],
-                    [ "PasteText", "PasteFromWord", "-", "Undo", "Redo" ],
-                    [ "Find", "Replace", "-", "Scayt" ],
-                    [ "Image", "Flash", "oembed", "Table", "HorizontalRule", "SpecialChar" ],
-                    "/",
-                    [ "Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript", "-", "RemoveFormat" ],
-                    [ "NumberedList", "BulletedList", "-", "Outdent", "Indent", "-", "Blockquote", "CreateDiv", "-",
-                      "JustifyLeft", "JustifyCenter", "JustifyRight", "JustifyBlock" ],
-                    [ "Link", "Unlink", "Anchor" ],
-                    "/",
-                    [ "Styles", "Format", "Font", "FontSize" ],
-                    [ "TextColor", "BGColor" ],
-                    [ "Maximize", "ShowBlocks" ],
-                    [ "About" ]
-                ]});
+                CKEDITOR.replace("{$this->htmlName}");
             </script>
 HTML;
-        // Из стандартного комплекта кнопок были исключены следующие:
-        // 'Save', 'NewPage', 'DocProps', 'Cut', 'Copy', 'Paste', 'SelectAll', 'Form', 'Checkbox',
-        // 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField',
-        // 'Smiley',  'PageBreak', 'Iframe'
-
         return $html;
     }
 
