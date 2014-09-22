00ceed53d41af5e7410168413afa10c211d3577d
heahoh <heahoh@gmail.com>
2014-09-22 18:57:00 +0400

    FIX: при отключении минификатора в админке - удаляем минифицированные файлы

--- Structure/Service/SiteData/Action.php
+++ Structure/Service/SiteData/Action.php
@@ -8,6 +8,14 @@
 
     if (isset($_POST['edit'])) {
         $file->saveFile(DOCUMENT_ROOT . '/' . $config->cmsFolder . '/site_data.php');
+        if (!isset($_POST['general_cms_enabledMin'])) {
+            if (file_exists(DOCUMENT_ROOT.'/js/all.min.js')) {
+                unlink(DOCUMENT_ROOT.'/js/all.min.js');
+            }
+            if (file_exists(DOCUMENT_ROOT.'/css/all.min.css')) {
+                unlink(DOCUMENT_ROOT.'/css/all.min.css');
+            }
+        }
     }
 
     echo $file->showEdit();
