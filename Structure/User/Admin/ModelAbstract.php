a2a64a05223f4021a476ef8d38c16b19ac0fb12f
Paul Nikitin <paulnikitin@gmail.com>
2014-09-23 11:26:06 +0400

    FIX: удаление пользователей

--- Structure/User/Admin/ModelAbstract.php
+++ Structure/User/Admin/ModelAbstract.php
@@ -9,7 +9,7 @@ class ModelAbstract extends \Ideal\Core\Admin\Model
     public function delete()
     {
         $db = Db::getInstance();
-        $db->delete($this->_table, $this->pageData['ID']);
+        $db->delete($this->_table)->where('ID=:id', array('id' => $this->pageData['ID']))->exec();
         // TODO сделать проверку успешности удаления
         return 1;
     }
