8f22ae89086c18621abf674e1a3a6bd84d45b3c1
help2 <help2@neox.ru>
2014-10-01 15:27:40 +0400

    Сделано дополнительное разделение методов. Изменена системы вывода сообщений. Создана сессия для хранения данных между ajax запросами

--- Structure/Service/UpdateCms/AjaxController.php
+++ Structure/Service/UpdateCms/AjaxController.php
@@ -29,26 +29,16 @@ class AjaxController extends \Ideal\Core\AjaxController
 
         $getFileScript = $this->srv . '/get.php';
 
-        // Файл лога обновлений
-        $log = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . 'update.log';
-
-        if (!file_exists($log)) {
-            $this->updateModel->uExit('Файл лога обновлений не существует ' . $log);
-        }
-
-        if (file_put_contents($log, '', FILE_APPEND) === false) {
-            $this->updateModel->uExit('Файл ' . $log . ' недоступен для записи');
-        }
 
         if (is_null($config->cms['tmpFolder']) || ($config->cms['tmpFolder'] == '')) {
-            $this->updateModel->uExit('В настройках не указана папка для хранения временных файлов');
+            $this->updateModel->uExit('В настройках не указана папка для хранения временных файлов', true);
         }
 
         // Папка для хранения загруженных файлов обновлений
         $uploadDir = DOCUMENT_ROOT . $config->cms['tmpFolder'] . '/update';
         if (!file_exists($uploadDir)) {
             if (!mkdir($uploadDir, 0755, true)) {
-                $this->updateModel->uExit('Не удалось создать папку' . $uploadDir);
+                $this->updateModel->uExit('Не удалось создать папку' . $uploadDir, true);
             }
         }
 
@@ -57,7 +47,7 @@ class AjaxController extends \Ideal\Core\AjaxController
         define('SETUP_DIR', $uploadDir . '/setup');
         if (!file_exists(SETUP_DIR)) {
             if (!mkdir(SETUP_DIR, 0755, true)) {
-                $this->updateModel->uExit('Не удалось создать папку' . SETUP_DIR);
+                $this->updateModel->uExit('Не удалось создать папку' . SETUP_DIR, true);
             }
         }
 
@@ -68,51 +58,108 @@ class AjaxController extends \Ideal\Core\AjaxController
             )
         );
 
-        // todo Сделать защиту от хакеров на POST-переменные
         if (!isset($_POST['version']) || !isset($_POST['name'])) {
-            $this->updateModel->uExit('Непонятно, что обновлять. Не указаны version и name');
+            $this->updateModel->uExit('Непонятно, что обновлять. Не указаны version и name', true);
+        } else {
+
+            $this->updateModel->setUpdate($_POST['name'], $_POST['version']);
+        }
+
+        // Создаём сессию для хранения данных между ajax запросами
+        session_start();
+        if (isset($_SESSION['update'])) {
+            if ($_SESSION['update']['name'] != $this->updateModel->updateName ||
+                $_SESSION['update']['version'] != $this->updateModel->updateVersion) {
+                unset($_SESSION['update']);
+            }
+        }
+        if (!isset($_SESSION['update'])) {
+            $_SESSION['update'] = array(
+                'name' => $this->updateModel->updateName,
+                'version' => $this->updateModel->updateVersion,
+            );
         }
     }
 
+    /**
+     * Загрузка архива с обновлениями
+     */
     public function ajaxDownloadAction()
     {
-        // Скачиваем и распаковываем архив с обновлениями
-        $this->updateModel->downloadUpdate($_POST['name'], $_POST['version']);
-
-        // todo разбить по отдельным вызовам
-        $this->ajaxUpdateAction();
-        $this->ajaxFinishAction();
+        // Скачиваем архив с обновлениями
+        $_SESSION['update']['archive'] = $this->updateModel->downloadUpdate();
+        exit(json_encode(true));
     }
 
+    // Распаковка архива с обновлением
     public function ajaxUnpackAction()
     {
-
+        $archive = isset($_SESSION['update']['archive']) ? $_SESSION['update']['archive'] : null;
+        if (!$archive) {
+            $this->updateModel->uExit('Неполучен путь к файлу архива', true);
+        }
+        $result = $this->updateModel->unpackUpdate($archive);
+        exit(json_encode($result));
     }
 
-    public function ajaxUpdateAction()
+    /**
+     * Замена старого каталога на новый
+     */
+    public function ajaxSwapAction()
+    {
+        $_SESSION['oldFolder'] = $this->updateModel->swapUpdate();
+        exit(json_encode(true));
+    }
+
+    /**
+     * Получение скриптов, которые необходимо выполнить для перехода на новую версию
+     */
+    public function ajaxGetUpdateScriptAction()
     {
         // Запускаем выполнение скриптов и запросов
-        $this->updateModel->updateScripts($_POST['name'], $_POST['version']);
+        $_SESSION['scripts'] = $this->updateModel->getUpdateScripts();
+        $this->updateModel->uExit(null, false, array('count' =>count($_SESSION['scripts'])));
     }
 
+    /**
+     * Выполнение одного скрипта из списка полученных скриптов
+     */
+    public function ajaxRunScriptAction()
+    {
+        if (!isset($_SESSION['scripts'])) {
+            exit(json_encode(true));
+        }
+        // Получаем скрипт, выполняемый в текущем ajax запросе
+        $script = array_shift($_SESSION['scripts']);
+        // Если все скрипты были выполнены ранее, возвращаем false
+        if (!$script) {
+            exit(json_encode(true));
+        }
+        // Запускаем выполнение скриптов и запросов
+        $this->updateModel->runScript($script);
+        $this->updateModel->uExit('Выполнен скрипт: ' . $script, false);
+        exit(json_encode(true));
+    }
+
+    /**
+     * Последний этап выполнения обновления
+     */
     public function ajaxFinishAction()
     {
         // Модуль установился успешно, делаем запись в лог обновлений
-        $this->updateModel->writeLog('Installed ' . $_POST['name'] . ' v. ' . $_POST['version']);
+        $this->updateModel->writeLog(
+            'Installed ' . $this->updateModel->updateName . ' v. ' . $this->updateModel->updateVersion
+        );
 
-        // Определяем путь к тому что мы обновляем, cms или модули
-        $config = Config::getInstance();
-        if ($_POST['name'] == "Ideal-CMS") {
-            // Путь к cms
-            $updateCore = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . "Ideal";
-        } else {
-            // Путь к модулям
-            $updateCore = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/' . "Mods" . '/' . $_POST['name'];
+        // Получаем раздел со старой версией
+        $oldFolder = isset($_SESSION['update']['oldFolder']) ? $_SESSION['update']['oldFolder'] : null;
+        $oldFolderError = '';
+        if (!$oldFolder) {
+            $oldFolderError = ' Не удалось удалить раздел со старой версией.';
         }
-
         // Удаляем старую папку
-        $this->updateModel->removeDirectory($updateCore . '_old');
+        $this->updateModel->removeDirectory($oldFolder);
 
-        $this->updateModel->uExit('Обновление завершено успешно');
+        $this->updateModel->uExit('Обновление завершено успешно' . $oldFolderError, false);
     }
 }
