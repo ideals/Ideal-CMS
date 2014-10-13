5d6fd821cf9392942711d56c5b2802663a456f26
help2 <help2@neox.ru>
2014-10-13 15:56:50 +0400

    Исправлено название файла с классом минификатора

--- setup/front/js/min.gen.php
+++ setup/front/js/min.gen.php
@@ -10,7 +10,7 @@
 if (!isset($_REQUEST['js']) || empty($_REQUEST['js'])) {
     die;
 }
-require_once('../[[CMS]]/Ideal/Library/Minifier/Minifier.php');
+require_once('../[[CMS]]/Ideal/Library/Minifier/class.magic-min.php');
 
 // Абсолютный адрес корня сервера, не должен оканчиваться на слэш
 define('DOCUMENT_ROOT', getenv('SITE_ROOT') ? getenv('SITE_ROOT') : $_SERVER['DOCUMENT_ROOT']);
