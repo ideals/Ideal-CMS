a85a0916c5ec5c17b9e958fb27787174b178d5d6
help2 <help2@neox.ru>
2014-10-21 17:06:50 +0400

    FIX: добавлено задание IP адреса

--- Library/YandexXML/Yandex.php
+++ Library/YandexXML/Yandex.php
@@ -649,6 +649,7 @@ class Yandex
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_INTERFACE, gethostbyname($_SERVER['SERVER_NAME']));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $xml->asXML());
         curl_setopt($ch, CURLOPT_POST, true);
         $data = curl_exec($ch);
