f7a8b3cfae65b85db9d986fb04569c6cab9512b3
Paul Nikitin <paulnikitin@gmail.com>
2015-02-02 22:40:08 +0300

    FIX: замена в установочном site_data.php с одинарных кавычек на двойные

--- setup/front/cms/site_data.php
+++ setup/front/cms/site_data.php
@@ -1,25 +1,25 @@
 <?php
 // @codingStandardsIgnoreFile
 return array(
-    'domain' => '[[SITENAME]]', // Домен | Ideal_Text
-    'robotEmail' => 'robot@[[SITENAME]]', // Почтовый ящик, с которого будут приходить письма с сайта | Ideal_Text
-    'mailForm' => 'info@[[SITENAME]]', // Почтовый ящик менеджера сайта | Ideal_Text
-    'phone' => '(495) 123-45-67', // Телефон в шапке сайта | Ideal_Text
-    'urlSuffix' => '.html', // Стандартный суффикс URL | Ideal_Text
-    'allowResize' => '', // Разрешённые размеры изображений (по одному на строку) | Ideal_Area
+    'domain' => "[[SITENAME]]", // Домен | Ideal_Text
+    'robotEmail' => "robot@[[SITENAME]]", // Почтовый ящик, с которого будут приходить письма с сайта | Ideal_Text
+    'mailForm' => "info@[[SITENAME]]", // Почтовый ящик менеджера сайта | Ideal_Text
+    'phone' => "(495) 123-45-67", // Телефон в шапке сайта | Ideal_Text
+    'urlSuffix' => ".html", // Стандартный суффикс URL | Ideal_Text
+    'allowResize' => "", // Разрешённые размеры изображений (по одному на строку) | Ideal_Area
     'cms' => array( // CMS
-        'startUrl' => '[[SUBFOLDER_START_SLASH]]', // Начальная папка CMS | Ideal_Text
-        'tmpFolder' => '/tmp', // Путь к папке с временными файлами | Ideal_Text
-        'errorLog' => 'firebug', // Способ уведомления об ошибках | Ideal_Select | {"firebug":"FireBug","email":"отправлять на email менеджера","display":"отображать в браузере","comment":"комментарий в html-коде","file":"сохранять в файл notice.log"}
-        'adminEmail' => '[[CMSLOGIN]]', // Почта, на которую будут отправляться сообщения об ошибках | Ideal_Text
-        'dirMode' => '0755', // Режим доступа к папке "0755" | Ideal_Text
-        'fileMode' => '0644', // Режим доступа к файлу "0644" | Ideal_Text
-        'known404' => '', // Известные битые ссылки (формат .htaccess или регулярные выражения) | Ideal_Area
+        'startUrl' => "[[SUBFOLDER_START_SLASH]]", // Начальная папка CMS | Ideal_Text
+        'tmpFolder' => "/tmp", // Путь к папке с временными файлами | Ideal_Text
+        'errorLog' => "firebug", // Способ уведомления об ошибках | Ideal_Select | {"firebug":"FireBug","email":"отправлять на email менеджера","display":"отображать в браузере","comment":"комментарий в html-коде","file":"сохранять в файл notice.log"}
+        'adminEmail' => "[[CMSLOGIN]]", // Почта, на которую будут отправляться сообщения об ошибках | Ideal_Text
+        'dirMode' => "0755", // Режим доступа к папке "0755" | Ideal_Text
+        'fileMode' => "0644", // Режим доступа к файлу "0644" | Ideal_Text
+        'known404' => "", // Известные битые ссылки (формат .htaccess или регулярные выражения) | Ideal_Area
     ),
     'cache' => array( // Кэширование
-        'jsAndCss' => '0', // Объединение и минификация css и js файлов | Ideal_Checkbox
-        'templateSite' => '0', // Кэширование twig-шаблонов | Ideal_Checkbox
-        'templateAdmin' => '0', // Кэширование twig-шаблонов админской части | Ideal_Checkbox
-        'memcache' => '0', // Кэширование запросов к БД | Ideal_Checkbox
+        'jsAndCss' => "0", // Объединение и минификация css и js файлов | Ideal_Checkbox
+        'templateSite' => "0", // Кэширование twig-шаблонов | Ideal_Checkbox
+        'templateAdmin' => "0", // Кэширование twig-шаблонов админской части | Ideal_Checkbox
+        'memcache' => "0", // Кэширование запросов к БД | Ideal_Checkbox
     ),
 );
