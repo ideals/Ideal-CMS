db4aeee32f4fe2d6af0f13150859039f4295ff9c
Paul Nikitin <paulnikitin@gmail.com>
2015-02-10 10:33:27 +0300

    ADD: окно логина после ajax-запроса на сохранение данных (unstable)

--- Structure/User/Admin/ControllerAbstract.php
+++ Structure/User/Admin/ControllerAbstract.php
@@ -42,14 +42,47 @@ class ControllerAbstract extends \Ideal\Core\Admin\Controller
      */
     public function loginAction()
     {
+
+        // Проверяем что запрашивается json
+        $jsonResponse = false;
+        $pattern = "/.*json.*/i";
+        if (preg_match($pattern, $_SERVER['HTTP_ACCEPT'])) {
+            $jsonResponse = true;
+        }
+
+
         $user = User\Model::getInstance();
 
         // Проверяем правильность логина и пароля
-        if (isSet($_POST['user']) and isSet($_POST['pass'])) {
-            if ($user->login($_POST['user'], $_POST['pass'])) {
-                header('Location: ' . $_SERVER['REQUEST_URI']);
-                exit;
+        if (isset($_POST['user']) && isset($_POST['pass'])) {
+
+            // При ajax авторизации отдаём json ответы
+            if ($jsonResponse) {
+                if ($user->login($_POST['user'], $_POST['pass'])) {
+                    echo json_encode(array('login' => 'true'));
+                } else {
+                    echo json_encode(array(
+                        'errorResponse' => $user->errorMessage,
+                        'login' => 'false'
+                    ));
+                }
+            } else {
+                if ($user->login($_POST['user'], $_POST['pass'])) {
+                    header('Location: ' . $_SERVER['REQUEST_URI']);
+                }
             }
+            exit;
+        }
+
+        // Если запрашивается json при не авторизованном пользователе
+        // отдаём ответ инициализирующий показ формы авторизации
+        if ($jsonResponse) {
+            echo json_encode(
+                array(
+                    'errorResponse' => 'not Login',
+                )
+            );
+            exit;
         }
 
         $this->templateInit('Structure/User/Admin/login.twig');
