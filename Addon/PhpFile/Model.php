6f11958010286e2492b89341d4eebe90e17219b0
morwal <blablamorwal@gmail.com>
2015-03-10 16:53:19 +0500

    Добавил проверку перед подключением файла в аддоне типа 'PHP-файл'

--- Addon/PhpFile/Model.php
+++ Addon/PhpFile/Model.php
@@ -11,7 +11,11 @@ class Model extends \Ideal\Addon\AbstractModel
         if ($mode[3] == 'Site') {
             // Для фронтенда к контенту добавляется выполнение указанного файла
             if ($this->pageData['php_file'] != '') {
-                require DOCUMENT_ROOT . $this->pageData['php_file'];
+                if (file_exists(DOCUMENT_ROOT . $this->pageData['php_file'])) {
+                    require DOCUMENT_ROOT . $this->pageData['php_file'];
+                } else {
+                    $this->pageData['content'] = 'Не удалось подключить файл "' . DOCUMENT_ROOT . $this->pageData['php_file'] . '"<br />' . $this->pageData['content'];
+                }
             }
         }
         return $this->pageData;
