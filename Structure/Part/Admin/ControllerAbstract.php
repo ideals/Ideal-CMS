52184e2852fbfb7b904f016573b7b028b2f9d175
MORWAL <blablamorwal@gmail.com>
2015-03-13 15:39:09 +0500

    Добавлено поле выбора шаблона отображения и сопутствующий функционал

--- Structure/Part/Admin/ControllerAbstract.php
+++ Structure/Part/Admin/ControllerAbstract.php
@@ -24,38 +24,4 @@ class ControllerAbstract extends \Ideal\Core\Admin\Controller
 
         $this->view->pager = $this->model->getPager('page');
     }
-
-    public function showCreateTemplateAction()
-    {
-        $request = new Request();
-        $template = $request->template;
-        $templateModelName = Util::getClassName($template, 'Template') . '\\Model';
-        /* @var $model \Ideal\Core\Admin\Model */
-        $model = new $templateModelName('не имет значения');
-        $model->setFieldsGroup($request->name);
-        $model->setPageDataNew();
-        echo $model->getFieldsList($model->fields);
-        exit;
-    }
-
-    public function showEditTemplateAction()
-    {
-        $request = new Request();
-
-        $this->model->setPageDataById($request->id);
-        $pageData = $this->model->getPageData();
-
-        $template = $request->template;
-        $templateModelName = Util::getClassName($template, 'Template') . '\\Model';
-        $model = new $templateModelName($template, $pageData['prev_structure']);
-        $model->setFieldsGroup($request->name);
-        // Загрузка данных связанного объекта
-        if (isset($pageData['ID'])) {
-            $prevStructure = $pageData['prev_structure'] . '-' . $pageData['ID'];
-            $model->setPageDataByPrevStructure($prevStructure);
-        }
-
-        echo $model->getFieldsList($model->fields);
-        exit;
-    }
 }
