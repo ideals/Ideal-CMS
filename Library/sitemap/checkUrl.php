9f61696322e5aa5c62bce11f8401bdd8eaa9dd04
Семён Егоров <sam.egorov@gmail.com>
2015-04-09 11:44:26 +0300

    FIX: новый класс карты сайта

--- Library/sitemap/checkUrl.php
+++ Library/sitemap/checkUrl.php
@@ -1,24 +1,18 @@
 <?php
 namespace ParseIt;
 
-/**
- * Created by PhpStorm.
- * User: Sam
- * Date: 14.02.2015
- * Time: 15:15
- */
+header('Content-Type: text/html; charset=utf-8');
+
+
 class ParseIt
 {
-    /** Регулярное выражение для проверки url */
-    const URL = "/^[a-zA-Z0-9\-\.\:\/_]{3,}$/"; // обращение self::
-
     /** Регулярное выражение для поиска ссылок */
     const LINK = "/<[Aa][^>]*[Hh][Rr][Ee][Ff]=['\"]?([^\"'>]+)[^>]*>/";
 
-    /** Регулярное выражение для поиска комментариев */
-    const COMMENT = "/<\!--.*-->/imsU";
+    /** @var string Время начала работы скрипта */
+    private $startUrl = 'http://linkinpark.com/';
 
-    /** @var  float Время начала работы скрипта */
+    /** @var float Время начала работы скрипта */
     private $start;
 
     /** @var string Переменная содержащая адрес главной страницы сайты */
@@ -30,7 +24,7 @@ class ParseIt
     /** @var string Имя хоста без www */
     private $nameUrl;
 
-    /** @var  array Массив с частями переданной ссылки */
+    /** @var array Массив с частями переданной ссылки */
     private $url = array();
 
     /** @var  array Массив НЕпроверенных ссылок */
@@ -39,8 +33,11 @@ class ParseIt
     /** @var array Массив проверенных ссылок */
     private $checked = array();
 
-    /** @var resource Поток для времнного файла  */
+    /** @var resource Поток для времнного файла */
     private $tmp;
+
+    /** @var  string Путь к времнному файлу */
+    private $tmp_file;
 
     private $options = array(
         CURLOPT_RETURNTRANSFER => true, //  возвращать строку, а не выводить в браузере
@@ -49,140 +46,189 @@ class ParseIt
         CURLOPT_FOLLOWLOCATION => true, // следовать по любому заголовку Location
         CURLOPT_ENCODING => "", // декодировать запрос используя все возможные кодировки
         CURLOPT_AUTOREFERER => true, // автоматическая установка поля referer в запросах, перенаправленных Location
-        CURLOPT_CONNECTTIMEOUT => 1, // кол-во секунд ожидания при соединении (мб лучше CURLOPT_CONNECTTIMEOUT_MS)
-        CURLOPT_TIMEOUT => 1, // максимальное время выполнения функций cURL функций
-        CURLOPT_MAXREDIRS => 2, // максимальное число редиректов
+        CURLOPT_CONNECTTIMEOUT => 4, // кол-во секунд ожидания при соединении (мб лучше CURLOPT_CONNECTTIMEOUT_MS)
+        CURLOPT_TIMEOUT => 4, // максимальное время выполнения функций cURL функций
+        CURLOPT_MAXREDIRS => 10, // максимальное число редиректов
+    );
+
+    private $excess = array(
+        '/users',
+        '/account'
     );
 
     public function __construct()
     {
-        //Подгрузка конфига
 
-        //время начала работы скрипта
+        // Время начала работы скрипта
         $this->start = microtime(1);
 
-        $this->prepareUrl($_POST['site']);
-    }
+        // Подгрузка конфига
+        $config = "sitemap.php";
 
-    /** Метод для обработки полученной ссылки */
-    private function prepareUrl($url)
-    {
-        // Проверка регулярным выражением
-        if (!preg_match(self::URL, $url)) {
-            echo "Некорректный url\n";
-            exit();
-        }
-        // Разбиение на части
-        $this->url = parse_url($url);
-
-        $this->checkSiteMap();
-    }
-
-    /** Метод для проверки существования карты сайта */
-    private function checkSiteMap()
-    {
-        //для названия файла c картой сайта вырезаем 'www.' из хоста
-        if (strripos($this->url['host'], "www.") !== false) {
-            $this->nameUrl = str_replace("www.", "", $this->url['host']);
+        if (!file_exists($config)) {
+            $this->info('Конфигурационный файл не найден!', 1);
         } else {
-            $this->nameUrl = $this->url['host'];
+            if ($config != '') {
+                $this->config = require('sitemap.php');
+                rtrim($this->config['website'], '/');
+            }
+        }
+        // Проверка существования файла sitemap.xml и его даты
+        if ($this->isSiteMapExist()) {
+            echo 'Карта сайта уже создана';
+            exit;
         }
 
-        // Для обхода ссылок лучше всегда обрезать "www."
-        // Так проще обрабатывать полученные сслыки
-        $this->url['host'] = $this->nameUrl;
+        //list($this->links, $this->checked) = $this->loadParsedUrls();
+        $this->loadParsedUrls();
 
-        $filename ="C:\\www\\parse-it\\sitemaps\\".$this->nameUrl."_".date("j-n-Y.")."txt";
-
-        //проверяем существование карты сайта сегодняшнего дня
-        if (file_exists($filename)) {
-            echo "Карта сайта уже есть\n";
-            exit();
+        if ((count($this->links) == 0) && (count($this->checked) == 0)) {
+            // Если это самое начало сканирования, добавляем в массив для сканирования первую ссылку
+            $this->links[$config['']] = 0;
         }
 
-        $this->accessCheck();
-
+        $this->run();
     }
 
-    /** Метод для проверки доступнности введенного сайта */
-    private function accessCheck()
+    protected function info($mes, $status)
     {
-        //создаем абсолютную ссылку с хостом
-        $this->host = 'http://'.$this->url['host'].'/';
-
-        //переменная для достраивания ссылок, начинающихся со '/'
-        $this->begin = "http://".$this->url['host'];
-
-        $headers = get_headers($this->host); // получаем заголовки сайта
-
-        //если сайт не доступен, выводим соответствующее сообщение
-        if (!in_array("HTTP/1.1 200 OK", $headers) and !in_array("HTTP/1.0 200 OK", $headers)) {
-            echo "Введенный сайт не доступен\n";
+        // $status: 0 - продолжить работу, 1 - остановить скрипт
+        echo $mes . "</br>";
+        if ($status == 1) {
             exit();
         }
-        $this->checkTempFile();
     }
 
-    /** Метод для проверки существования временного файла */
-    private function checkTempFile()
+    protected function isSiteMapExist()
     {
-        //проверяем существование временного файла
-        $filename = "C:\\www\\parse-it\\tmp\\".$this->nameUrl."_tmp.txt";
-        //Если файл существует считываем его
-        if (file_exists($filename)) {
-            $arr = file_get_contents($filename);
+        // todo Это заглушка, надо реализовать функционал СДЕЛАНО
+
+        $xmlFile = $this->config['pageroot'] . $this->config['sitemap_file'];
+
+        // Проверяем существует ли файл и доступен ли он для чтения и записи
+        if (file_exists($xmlFile)) {
+            if (!is_readable($xmlFile)) {
+                $this->info('Карта сайта не доступна для чтения!', 1);
+                return false;
+            }
+            if (!is_writable($xmlFile)) {
+                $this->info('Карта сайта не доступна!', 1);
+                return false;
+            }
+            return true;
+        } else {
+            // Файла нет, пытаемся создать
+            if (file_put_contents($xmlFile, '') === false) {
+                // Создать файл не получилось
+                $this->info('Не удалось создать файл для карты сайта!', 1);
+                return false;
+            }
+        }
+        // Проверяем, обновлялась ли сегодня карта сайта
+        if (date('d:m:Y', filemtime($xmlFile)) == date('d:m:Y')) {
+            $this->info("Warning! File {$xmlFile} have current date and skip in cron", 1);
+        }
+
+        return true;
+    }
+
+
+    /** Метод для загрузки распарсенных данных из временного файла */
+    protected function loadParsedUrls()
+    {
+        // Если существует файл хранения временных данных сканирования,
+        // Данные разбиваются на 2 массива: пройденных и непройденных ссылок
+        $tmpFile = $this->config['pageroot'] . $this->config['tmp_file'];
+        if (file_exists($tmpFile)) {
+            $arr = file_get_contents($tmpFile);
             $this->links = unserialize($arr);
-            //Разбиваем массив на пройденные и непройденные ссылки
-            foreach ($this->links as $v => $k) {
-                if ($k == 1) {
-                    $this->checked[$v] = $k;
-                    unset($this->links[$v]);
+            unset($arr);
+            /* todo массив вида [ссылка] => array(checked(1 или 0), position) */
+            foreach ($this->links as $k => $v) {
+                if ($v['checked'] == 1) {
+                    $this->checked[$k] = $v;
+                    unset($this->links[$k]);
                 }
             }
-            $this->tmp = fopen($filename, "w");
-        } else {
-            // иначе создаем его
-            $this->tmp = fopen($filename, "w+");
-            // записываем первую сслыку в массив
-            $this->links[$this->host] = 0;
-            // Если переданная ссылка не главная страница, тоже добавляем её
-            if (isset($this->url['path']) and $this->url['path'] != '/') {
-                //Добавляем в массив ссылку, достроенную до абсолютной
-                $this->links[$this->begin.$this->url['path']] = 0;
-            }
         }
-        $this->run();
+    }
+
+    /** Метод для сохранения распарсенных данных во временный файл */
+    protected function saveParsedUrls()
+    {
+        $result = array_merge($this->links, $this->checked);
+        $result = serialize($result);
+
+        $tmp_file = $this->config['pageroot'] . $this->config['tmp_file'];
+
+        $fp = fopen($tmp_file, "w");
+
+        fwrite($fp, $result);
+
+        fclose($fp);
     }
 
     /** Метод основного цикла для сборка карты сайта и парсинга товаров */
     private function run()
     {
         /** Массив links вида [ссылка] => пометка(не играет роли) */
-        /** Массив checked вида [ссылка] => пометка о том является ли ссылка корректной (1 - да, 0 - нет(404)) */
+        /** Массив checked вида [ссылка] => пометка о том является ли ссылка корректной (1 - да, 0 - нет) */
         while (count($this->links) > 0) {
             $time = microtime(1);
-            /** Проверка не прошло ли 60 секунд
-             *  и есть ли еще время на одну итерацию(2 секунды) */
-            if (($time - $this->start) < 60.00 && (60.00 - ($time - $this->start)) > 1.00) {
-                // Устанавливаем указатель на 1-й элемент
-                reset($this->links);
-
-                // Извлекаем ключ текущего элемента (то есть ссылку)
-                $k = key($this->links);
-
-                //Передаем ссылку на парсинг
-                $this->getUrl($k);
-            } else {
-                $result = array_merge($this->links, $this->checked);
-                $result = serialize($result);
-                fwrite($this->tmp, $result);
-                fclose($this->tmp);
-                echo "timeout";
-                exit();
+            // todo переделать условие на нормальное, зависящее от времени выполнения скрипта и времени на сохранение данных
+            if (($time - $this->start) > 58.00) {
+                break;
             }
+
+            // Устанавливаем указатель на 1-й элемент
+            reset($this->links);
+
+            // Извлекаем ключ текущего элемента (то есть ссылку)
+            $k = key($this->links);
+
+            // Получаем контент страницы
+            $content = $this->getUrl($k);
+
+            // Парсим ссылки из контента
+            $urls = $this->parseLinks($content);
+
+            // Добавляем ссылки в массив $this->links
+            $this->addLinks($urls, $k);
+
+            // Добавляем текущую ссылку в массив пройденных ссылок
+            $this->checked[$k] = 1;
+
+            // И удаляем из массива непройденных
+            unset ($this->links[$k]);
+
         }
-        echo "Карта сайта успешно создана!\n";
+
+        if (count($this->links) > 0) {
+            $this->saveParsedUrls();
+            echo "Всего пройденных ссылок: " . count($this->checked) . "\n" . "</br>";
+            echo "Всего непройденных ссылок: " . count($this->links) . "</br>";
+            echo($time - $this->start);
+            exit();
+        }
+        $this->saveSiteMap();
+    }
+
+
+    protected function saveSiteMap()
+    {
+        // todo Сохранение xml-карты сайта
         fclose($this->tmp);
+        // Удаляем временный файл
+        unlink($this->tmp_file);
+
+        $filename = "/var/www/html/sitemaps/" . $this->nameUrl . "_" . date("j-n-Y.") . "txt";
+        $fp = fopen($filename, "w");
+        $result = serialize($this->checked);
+        fwrite($fp, $result);
+        fclose($fp);
+
+        //echo "Карта сайта успешно создана!\n";
+        echo "done";
     }
 
     /* Метод для получения html кода и парсинга текущей страницы в основном цикле */
@@ -200,6 +246,7 @@ class ParseIt
             //Если страница имеет статус 404 добавляем её в массив пройденных с соответствующим статусом
             $this->checked[$k] = 1;
             unset ($this->links[$k]);
+
             return 0;
         }
 
@@ -209,43 +256,138 @@ class ParseIt
 
         $res = substr($res, $header_size); //вырезаем html код страницы
 
-        $res = preg_replace(self::COMMENT, '', $res); // удаление комментариев из страницы
+        return $res;
+    }
 
-        //получение всех ссылок со страницы
-        preg_match_all(self::LINK, $res, $urls);
 
-        $this->processingLinks($k, $urls[1]);
+    protected function parseLinks($content)
+    {
+        // Получение всех ссылок со страницы
+        preg_match_all(self::LINK, $content, $urls);
+
+        return $urls;
     }
 
     /* Метод для обработки полученных ссылок в результате парсинга страницы */
-    private function processingLinks($k, $urls)
+    private function addLinks($urls, $current)
     {
-        foreach ($urls as $val) {
-            //Переменная для флага, является ли данный url - ссылкой на наш сайт
-            $right_url = false;
+        foreach ($urls as $url) {
+            $link = $this->getAbsoluteUrl($url, $current);
 
-            //Вырезаем "www."
-            $val = str_replace("www.", "", $val);
+            if ($this->isExternalLink($link)) {
+                // Пропускаем ссылки на другие сайты
+                continue;
+            }
 
-            //Если ссылка начинается со "/" то достраиваем и отмечаем её
-            if (strpos($val, '/') === 0) {
-                $val = 'http://'.$this->url['host'].$val;
-                $right_url = true;
+            if (isset($this->links[$link])) {
+                // Пропускаем уже добавленные ссылки
+                continue;
             }
-            //Если существует абсолютная ссылка на данный сайт то отмечаем её
-            if (strpos($val, "http://".$this->url['host']) === 0) {
-                $right_url = true;
+
+            $this->links[$link] = 0;
+        }
+    }
+
+    protected function getAbsoluteUrl($link, $current)
+    {
+        if (preg_match(',^(https?://|ftp://|mailto:|news:|javascript:|telnet:|callto:|skype:),i', $link)) {
+            // hostname is not the same (with/without www) than the one used in the link
+            // Если ссылка начинается с http
+            if (substr($link, 0, 4) == 'http') {
+                // То разбиваем её на части
+                $url = parse_url($link);
+                // Если хост из данной ссылке не равен заданному хосту,
+                // но они равны при добавлении к одному из них www
+                if ($url['host'] != $this->host && ((("www." . $url['host']) == $this->host) && $this->withWWW == true || ($url['host'] == ("www." . $this->host)) && $this->withWWW == false)) {
+                    // заменяем хост из ссылки заданным хостом (из конфига)
+                    $link = str_replace($url['host'], $this->host, $link);
+                }
+                // Если передан только хост, то добавляем в конце "/"
+                if (!array_key_exists('path', $url) || $url['path'] == '' && substr($link, -1) != '/') {
+                    $link .= '/';
+                }
             }
-            //Если такой ссылки массиве еще нет и ссылка ведет на наш сайт, то добавляем её
-            if (!isset($this->links[$val]) and !isset($this->checked[$val]) and $right_url === true) {
-                $this->links[$val] = 0;
+            return $link;
+        }
+
+        // Разбираем текущую ссылку на компоненты
+        $url = parse_url($current);
+
+        // Если последний символ в "path" текущей это слэш "/"
+        if ($url['path']{strlen($url['path']) - 1} == '/') {
+            // Промежуточная директория равна "path" текущей ссылки без слэша
+            $dir = substr($url['path'], 0, strlen($url['path']) - 1);
+        } else {
+            // Устанавливаем родительский элемент
+            $dir = dirname($url['path']);
+            // Если ссылка начинается с "?"
+            if ($link{0} == '?') {
+                // То обрабатываемая ссылка равна последней части текущей ссылки + сама ссылка
+                $link = basename($url['path']) . $link;
             }
         }
-        // Добавляем текущую ссылку в массив пройденных ссылок
-        $this->checked[$k] = 1;
-        // И удаляем из массива непройденных
-        unset ($this->links[$k]);
+
+        // Если ссылка начинается со слэша
+        if ($link{0} == '/') {
+            // Обрезаем слэш
+            $link = substr($link, 1);
+            // Убираем промежуточный родительский элемент
+            $dir = '';
+        }
+
+        // Если выбранный хост из ссылки не равен хосту из конфига,
+        // но он все же есть в обрабатываемой ссылке, то задаем хост из конфига как текущий.
+        if ($url['host'] != $this->host && (strpos($url['host'], $this->host) != false || strpos($this->host, $url['host']) != false)) {
+            $url['host'] = $this->host;
+        }
+
+        // Если ссылка начинается с "./"
+        if (substr($link, 0, 2) == './') {
+            $link = substr($link, 2);
+        } else {
+            // До тех пор пока ссылка начинается с "../"
+            while (substr($link, 0, 3) == '../') {
+                // Обрезаем "../"
+                $link = substr($link, 3);
+                // Устанавливаем родительскую директорию равную текущей, но обрезая её с последнего "/"
+                $dir = substr($dir, 0, strrpos($dir, '/'));
+            }
+        }
+
+        // Если задано base -  добваляем его
+        if (strlen($this->base)) {
+            return $this->base . urldecode($link);
+        }
+
+        // Возвращаем абсолютную ссылку
+        return sprintf('%s://%s%s/%s', $url['scheme'], $url['host'], $dir, urldecode($link));
+
+    }
+
+    protected function isExternalLink($link)
+    {
+        // Если ссылка на приложение - пропускаем её
+        if (preg_match(',^(ftp://|mailto:|news:|javascript:|telnet:|callto:|skype:),i', $link)) {
+            return true;
+        }
+
+        $url = parse_url($link);
+
+        //Начальная директория - хост из конфига
+        $startDir = $this->host;
+        // Текущая директория: хост переданной ссылки
+        $curentDir = $url["host"];
+
+        // Если текущий хост с начала сторки равен хосту из конфига возвращаем true
+        $extLink = (substr($curentDir, 0, strlen($startDir)) == $startDir);
+        // Если хосты равны
+        if ($extLink == true) {
+            // Возвращаем false, т.е. текущая ссылка не внешняя
+            return false;
+        } else {
+            return false;
+        }
     }
 }
-// Инициализация создания карты сайта
+
 $A = new ParseIt();
