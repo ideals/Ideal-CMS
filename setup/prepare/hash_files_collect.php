606838a024f205bc5177f1fe6eecb52171ba7650
MORWAL <blablamorwal@gmail.com>
2015-04-30 14:56:30 +0500

    FIX Сбор хэшей работает корректно из командной строки. Добавлена возможность передачи полного пути до папки CMS аргументом

--- setup/prepare/hash_files_collect.php
+++ setup/prepare/hash_files_collect.php
@@ -1,16 +1,21 @@
 <?php
-$cmsFolder = __DIR__ . '/../../..';
+$cmsFolder = __DIR__ . '/../..';
+
+// Если передан аргумент содержащий путь до папки CMS, то используем его
+if (isset($_SERVER['argv'][1])) {
+    $cmsFolder = $_SERVER['argv'][1];
+}
 
 // Подключаем файлы, которые нужны для работы класса AjaxController
-require $cmsFolder . '/Ideal/Core/AjaxController.php';
+require $cmsFolder . '/Core/AjaxController.php';
 
 // Подключаем класс проверки целостности файлов
-require $cmsFolder . '/Ideal/Structure/Service/CheckCmsFiles/AjaxController.php';
+require $cmsFolder . '/Structure/Service/CheckCmsFiles/AjaxController.php';
 
 // Собираем хэши файлов
-$scanFolder = $cmsFolder . '/Ideal';
+$scanFolder = $cmsFolder;
 $systemFiles = Ideal\Structure\Service\CheckCmsFiles\AjaxController::getAllSystemFiles($scanFolder, $cmsFolder);
 
 // Записываем данные в файл информации о хэшах файлов системы
 $file = "<?php\n// @codingStandardsIgnoreFile\nreturn " . var_export($systemFiles, true) . ";\n";
-file_put_contents($cmsFolder . '/Ideal/setup/prepare/hash_files.php', $file);
+file_put_contents($cmsFolder . '/setup/prepare/hash_files.php', $file);
