d22df51462acca7930ee87d484dbae11799c44f5
MORWAL <blablamorwal@gmail.com>
2015-05-18 19:04:39 +0500

    В процессе работы над задачей 'Улучшение обработки 404-ых ошибок'

---
+++ Structure/Service/Known404/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'name' => '404 ошибки',
+    'pos' => 70,
+    'info' => '',
+    'structure' => 'Ideal_Service',
+    'ID' => 'Ideal_Known404',
+);
---
+++ setup/front/cms/known404.php
@@ -0,0 +1,3 @@
+<?php
+// @codingStandardsIgnoreFile
+return array();
