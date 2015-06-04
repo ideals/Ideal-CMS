a12ddd8826fa8ce3b746c6e08bddd1896785a9e4
MORWAL <blablamorwal@gmail.com>
2015-06-04 13:15:25 +0500

    Выбор одного/нескольких изображений из окна 'CKFinder' и добавление их к списку фотографий в текстовом поле

--- Field/ImageGallery/script.js
+++ Field/ImageGallery/script.js
@@ -1,2 +1,25 @@
-$(document).ready(function () {
-});
+// Открывает окно CKFinder для возможности выбора изображений
+function imageGalleryShowFinder(fieldSelector) {
+    var finder = new CKFinder();
+    finder.selectActionData = {"fieldSelector" : fieldSelector};
+    finder.basePath = '{{ cmsFolder }}/Ideal/Library/ckfinder/';
+    finder.selectActionFunction = imageGallerySetFileField;
+    finder.popup();
+}
+
+// Производит работу над выбранными изображениями
+function imageGallerySetFileField(fileUrl, data, allFiles) {
+
+    var urls = [];
+    $.each(allFiles, function (index, value) {
+        urls.push([value.url, '']);
+    });
+    var currentData = $('#' + data.selectActionData.fieldSelector).val();
+    // Если пока нет никаких данных по изображениям значит записываем только что выбранные
+    if (currentData == '') {
+        $('#' + data.selectActionData.fieldSelector).val(JSON.stringify(urls));
+    } else {
+        var currentDataArray = JSON.parse(currentData);
+        $('#' + data.selectActionData.fieldSelector).val(JSON.stringify(currentDataArray.concat(urls)));
+    }
+}
