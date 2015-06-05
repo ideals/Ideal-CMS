522ee63e9fb9cd203007d057eee50886d9e02955
Paul Nikitin <paulnikitin@gmail.com>
2015-06-05 10:59:46 +0300

    FIX: убрал tab_ID из HiddenController, сделал его принудительную установку при получении контента вкладки

--- Field/Hidden/Controller.php
+++ Field/Hidden/Controller.php
@@ -48,37 +48,4 @@ class Controller extends AbstractController
         . '" name="' . $this->htmlName
         . '" value="' . $this->getValue() . '">';
     }
-
-    /**
-     * Используется для присвоения значения скрытому полю идентификатора таба, если оно пустое (при добавлении нового
-     * аддона)
-     */
-    public function parseInputValue($isCreate)
-    {
-        // TODO переделать, чтобы использовать sqlAdd
-        $item = parent::parseInputValue($isCreate);
-        if (strpos($item['fieldName'], 'tab_ID') === false) {
-            return $item;
-        } else {
-            if (empty($item['value'])) {
-                list($tabID) = explode('_', $item['fieldName'], 2);
-                list(, $tabID) = explode('-', $tabID);
-                $item['sqlAdd'] = $this->getSqlAdd($tabID);
-            }
-            return $item;
-        }
-    }
-
-    /**
-     * Получение дополнительных sql-запросов для обновления идентификатора таба при создании нового аддона
-     *
-     * @param $tabID - идентификаор таба
-     * @return string
-     */
-    public function getSqlAdd($tabID)
-    {
-        // Удаляем все существующие связи владельца и элементов
-        $_sql = "UPDATE {{ table }} SET {$this->name}=$tabID WHERE ID='{{ objectId }}';";
-        return $_sql;
-    }
 }
