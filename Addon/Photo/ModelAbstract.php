b33f91691884e9e540260c5171fab19e920557a5
MORWAL <blablamorwal@gmail.com>
2015-06-05 13:52:53 +0500

    Добавлен вывод аддона 'Фотогалерея'

--- Addon/Photo/ModelAbstract.php
+++ Addon/Photo/ModelAbstract.php
@@ -1,7 +1,27 @@
 <?php
 namespace Ideal\Addon\Photo;
 
+use Ideal\Core\Config;
+use Ideal\Core\View;
+
 class ModelAbstract extends \Ideal\Addon\AbstractModel
 {
+    public function getPageData()
+    {
+        $this->setPageDataByPrevStructure($this->prevStructure);
 
+        $mode = explode('\\', get_class($this->parentModel));
+        if ($mode[3] == 'Site') {
+
+            $config = Config::getInstance();
+
+            $tplRoot = dirname(stream_resolve_include_path('Addon/Photo/index.twig'));
+            $View = new View($tplRoot, $config->cache['templateSite']);
+            $View->loadTemplate('index.twig');
+            $View->images = json_decode($this->pageData['images']);
+            $View->imagesRel = $this->fieldsGroup;
+            $this->pageData['content'] .= $View->render();
+        }
+        return $this->pageData;
+    }
 }
