643534eca9c98c6d60b8aa1d60eca6edc55f4125
Paul Nikitin <paulnikitin@gmail.com>
2015-06-08 21:13:45 +0300

    FIX: исправлены даты копирайтов

--- Field/DateSet/Controller.php
+++ Field/DateSet/Controller.php
@@ -3,7 +3,7 @@
  * Ideal CMS (http://idealcms.ru/)
  *
  * @link      http://github.com/ideals/idealcms репозиторий исходного кода
- * @copyright Copyright (c) 2012-2014 Ideal CMS (http://idealcms.ru/)
+ * @copyright Copyright (c) 2012-2015 Ideal CMS (http://idealcms.ru/)
  * @license   http://idealcms.ru/license.html LGPL v3
  */
 
