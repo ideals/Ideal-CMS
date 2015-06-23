6beae75036561bccbd5796d01e395666a1edf9b3
MORWAL <blablamorwal@gmail.com>
2015-06-23 10:26:10 +0500

    В инсталлятор структуры Ideal_Order дописано создание записи-ссылки на неё в справочниках (Ideal_DataList) с названием 'Заказы с сайта'

--- Structure/Order/install.php
+++ Structure/Order/install.php
@@ -2,7 +2,29 @@
 // Инициализируем доступ к БД
 $db = Ideal\Core\Db::getInstance();
 
+$config = Ideal\Core\Config::getInstance();
+
 $cfg = $config->getStructureByName('Ideal_Order');
+
+$dataListTable = $config->db['prefix'] . 'ideal_structure_datalist';
+
+$_sql = "SELECT MAX(pos) as maxPos FROM {$dataListTable}";
+$max = $db->select($_sql);
+$newPos = intval($max[0]['maxPos']) + 1;
 
 // Создание таблицы для справочника
 $db->create($config->db['prefix'] . 'ideal_structure_order', $cfg['fields']);
+
+$db->insert(
+    $dataListTable,
+    array(
+        'ID' => 1,
+        'prev_structure' => '0-3',
+        'structure' => 'Ideal_Referrer',
+        'pos' => $newPos,
+        'name' => 'Заказы с сайта',
+        'url' => 'zakazy-s-sajta',
+        'parent_url' => '---',
+        'annot' => ''
+    )
+);
