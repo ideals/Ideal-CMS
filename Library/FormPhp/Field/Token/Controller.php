a028c268633e4774cb5a344abf5705d8645ab020
Doctor-Zi <zeroicce@gmail.com>
2015-07-07 14:52:16 +0300

    FIX: проверка состояние сессии

--- Library/FormPhp/Field/Token/Controller.php
+++ Library/FormPhp/Field/Token/Controller.php
@@ -13,6 +13,9 @@ class Controller extends AbstractField
 {
     public function getInputText()
     {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
         $xhtml = ($this->xhtml) ? '/' : '';
         return '<input type="hidden" name="_token" value="' . crypt(session_id()) . '" ' . $xhtml . '>' . "\n";
     }
