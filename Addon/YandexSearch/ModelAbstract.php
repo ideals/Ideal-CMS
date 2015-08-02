b1712e286d5f9e04e4e3d63216bb08f948d95388
Paul Nikitin <paulnikitin@gmail.com>
2015-08-02 11:49:45 +0300

    FIX: улучшение работы поиска по сайту FIX: PSR-2

--- Addon/YandexSearch/ModelAbstract.php
+++ Addon/YandexSearch/ModelAbstract.php
@@ -1,103 +1,126 @@
 <?php
+/**
+ * Ideal CMS (http://idealcms.ru/)
+ *
+ * @link      http://github.com/ideals/idealcms репозиторий исходного кода
+ * @copyright Copyright (c) 2012-2015 Ideal CMS (http://idealcms.ru)
+ * @license   http://idealcms.ru/license.html LGPL v3
+ */
+
 namespace Ideal\Addon\YandexSearch;
 
+use Ideal\Addon;
 use Ideal\Core\Config;
 use Ideal\Core\View;
 use Ideal\Structure\User;
 use Ideal\Core\Request;
 use YandexXML\YandexXmlClient;
-use YandexXML\YandexXmlException;
 use Exception;
 
-class ModelAbstract extends \Ideal\Addon\AbstractModel
+/**
+ * Класс аддона, обеспечивающий поиск по сайту
+ *
+ * Содержит в себе обращение к сервису Яндекс.XML либо по настройкам аддона, либо по глобальным настройкам
+ * в CMS (файл config.php). Аддон содержит шаблон index.twig, подключаемый для генерации поля content аддона,
+ * поэтому аддон можно подлючать как обычный аддон Page, для которого никакой дополнительной кастомизации
+ * в общем шаблоне не требуется.
+ */
+class ModelAbstract extends Addon\AbstractModel
 {
-    /** @var int Общее количесвто результатов поиска */
-    protected $listCount = 0;
-
+    /**
+     * Получение данных аддона с выполнение всех действий (в данном случае — запроса к Яндексу)
+     *
+     * @return array Все данные аддона и сгенерированное поле content с отображаемым html-кодом
+     */
     public function getPageData()
     {
         $this->setPageDataByPrevStructure($this->prevStructure);
 
         $mode = explode('\\', get_class($this->parentModel));
 
-        if ($mode[3] == 'Site') {
-            $config = Config::getInstance();
-
-            $tplRoot = dirname(stream_resolve_include_path('Addon/YandexSearch/index.twig'));
-            $view = new View($tplRoot, $config->cache['templateSite']);
-            $view->loadTemplate('index.twig');
-
-            // Логин от сервиса Яндекс
-            $yandexLogin = trim($this->pageData['yandexLogin']);
-
-            // Ключ от сервиса Яндекс
-            $yandexKey = trim($this->pageData['yandexKey']);
-
-            // Адрес прокси скрипта
-            $proxyUrl = trim($this->pageData['proxyUrl']);
-            if (empty($proxyUrl)) {
-                $proxyUrl = trim($config->yandex['proxyUrl']);
-            }
-
-            // Номер отображаемой страницы
-            $request = new Request();
-            $page = intval($request->{'num'});
-            $page = ($page == 0) ? 1 : $page;
-            $page--;
-            
-            // Поисковый запрос
-            $request = new Request();
-            $query = trim(strval($request->{'search'}));
-
-            if (empty($query)) {
-                $this->pageData['content'] .= 'Пустой поисковый запрос';
-            } else {
-                if (empty($yandexLogin) || empty($yandexKey)) {
-                    $yandexLogin = trim($config->yandex['yandexLogin']);
-                    $yandexKey = trim($config->yandex['yandexKey']);
-                }
-
-                // Для фронтенда рендерим результат поиска
-                if (!empty($yandexLogin) && !empty($yandexKey) && !empty($query)) {
-                    $yandex = new YandexXmlClient($yandexLogin, $yandexKey);
-
-                    // Отлавливаем исключения и отдаём их на страницу
-                    try {
-                        $yandex->query($query)// устанавливаем поисковый запрос
-                        ->site($config->domain)// ограничиваемся поиском по сайту
-                        ->setProxyUrl($proxyUrl)
-                            ->page($page)
-                            ->limit($this->pageData['elements_site'])// результатов на странице
-                            ->request()                            // отправляем запрос
-                        ;
-                    } catch (YandexXmlException $e) {
-                        $this->pageData['content'] .= $e->getMessage();
-                    } catch (Exception $e) {
-                        $this->pageData['content'] .= $e->getMessage();
-                    }
-
-                    // Параметр необходимый для получения листалки
-                    $elementsSite = $this->pageData['elements_site'];
-                    $this->params['elements_site'] = !empty($elementsSite) ? $elementsSite : 15;
-
-                    $list = $yandex->results();
-                    $list = $this->view($list);
-                    $this->listCount = $yandex->total();
-
-                    // Передаём данные в шаблон для рендера поиска
-                    $view->parts = $list;
-                    $view->query = $query;
-                    $view->pager = $this->getPager('num');
-
-                } else {
-                    $this->pageData['content'] .= 'Поле логин или ключ от яндекса имеет пустое значене';
-                }
-            }
-            $this->pageData['content'] .= $view->render();
+        if ($mode[3] != 'Site') {
+            // Отображение поиска нужно только для фронтенда, в бэкенде просто возвращаем данные из БД
+            return $this->pageData;
         }
+
+        $config = Config::getInstance();
+
+        // Подключаем шаблон аддона
+        $tplRoot = dirname(stream_resolve_include_path('Addon/YandexSearch/index.twig'));
+        $view = new View($tplRoot, $config->cache['templateSite']);
+        $view->loadTemplate('index.twig');
+
+        // Логин и ключ от сервиса Яндекс
+        $yandexLogin = trim($this->pageData['yandexLogin']);
+        $yandexKey = trim($this->pageData['yandexKey']);
+
+        // Адрес прокси скрипта
+        $proxyUrl = trim($this->pageData['proxyUrl']);
+        if (empty($proxyUrl)) {
+            $proxyUrl = trim($config->yandex['proxyUrl']);
+        }
+
+        // Номер отображаемой страницы
+        $request = new Request();
+        $page = intval($request->{'num'});
+        $page = ($page == 0) ? 1 : $page;
+        $page--;
+
+        // Поисковый запрос
+        $request = new Request();
+        $query = trim(strval($request->{'query'}));
+
+        if (!empty($query)) {
+            if (empty($yandexLogin) || empty($yandexKey)) {
+                $yandexLogin = trim($config->yandex['yandexLogin']);
+                $yandexKey = trim($config->yandex['yandexKey']);
+            }
+
+            // Для фронтенда рендерим результат поиска
+            if (!empty($yandexLogin) && !empty($yandexKey) && !empty($query)) {
+                $yandex = new YandexXmlClient($yandexLogin, $yandexKey);
+
+                // Параметр необходимый для получения листалки
+                $elementsSite = $this->pageData['elements_site'];
+                $this->params['elements_site'] = !empty($elementsSite) ? $elementsSite : 15;
+
+                // Отлавливаем исключения и отдаём их на страницу
+                try {
+                    $yandex->query($query) // устанавливаем поисковый запрос
+                    ->site($config->domain) // ограничиваемся поиском по сайту
+                    ->setProxyUrl($proxyUrl)
+                        ->page($page)
+                        ->limit($this->params['elements_site']) // результатов на странице
+                        ->request()                             // отправляем запрос
+                    ;
+                } catch (Exception $e) {
+                    $view->message = $e->getMessage();
+                }
+
+                $list = $yandex->results();
+                $list = $this->view($list);
+
+                // Передаём данные в шаблон для рендера поиска
+                $view->total = $yandex->total();
+                $view->parts = $list;
+                $view->query = $query;
+                $view->pager = $this->getPager('num');
+
+            } else {
+                $view->message = 'Поле логин или ключ от яндекса имеет пустое значене';
+            }
+        }
+        $this->pageData['content'] .= $view->render();
+
         return $this->pageData;
     }
 
+    /**
+     * Построение списка страниц в виде массива на основании ответа яндекса в виде XML с выделением пассажей
+     *
+     * @param array $list Массив классов, содержащих найденные страницы
+     * @return array Массив найденных страниц, с выделенными пассажами
+     */
     protected function view($list)
     {
         $result = array();
@@ -113,10 +136,5 @@ class ModelAbstract extends \Ideal\Addon\AbstractModel
             }
         }
         return $result;
-    }
-
-    public function getListCount()
-    {
-        return $this->listCount;
     }
 }
