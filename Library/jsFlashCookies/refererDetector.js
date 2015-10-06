c38eb2abf88346ff1352a87a7604f04424253257
MORWAL <help7@neox.ru>
2015-10-06 12:22:48 +0500

    FIX: Теперь даже если отключен флеш и включено файловое кэширование происходит попытка получить реферер

--- Library/jsFlashCookies/refererDetector.js
+++ Library/jsFlashCookies/refererDetector.js
@@ -12,13 +12,7 @@ $(document).ready(function() {
 
         // Если удалось подключиться к хранилищу флеш куков
         onready: function () {
-            // Получаем значение реферера, которое было установлено на php
-            var refererCookiePhp = getCookie('referer');
-
-            // Если на php не было задано значение реферера, то инициализируем переменную с текущим значением
-            if (refererCookiePhp == undefined) {
-                refererCookiePhp = document.referrer;
-            }
+            var refererCookiePhp = getPhpCookieReferer();
 
             // Получаем значение флеш куки реферера
             var refererCookieFlash = mySwfStore.get('referer');
@@ -37,16 +31,13 @@ $(document).ready(function() {
                 } else {
                     currentRefererValue = refererCookiePhp;
                 }
-
-                var now = new Date();
-                var time = now.getTime();
-                var expireTime = time + 315360000;
-                now.setTime(expireTime);
-                document.cookie = 'referer=' + currentRefererValue + ';expires=' + now.toGMTString() + ';path=/';
+                setCoockieReferer(currentRefererValue);
             }
         },
         onerror: function () {
             console.log('У вас отключен флэш.');
+            var refererCookiePhp = getPhpCookieReferer();
+            setCoockieReferer(refererCookiePhp);
         }
     });
 
@@ -55,5 +46,30 @@ $(document).ready(function() {
             "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
         ));
         return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    function getPhpCookieReferer() {
+        // Получаем значение реферера, которое было установлено на php
+        var refererCookiePhp = getCookie('referer');
+
+        // Если на php не было задано значение реферера, то инициализируем переменную с текущим значением
+        if (refererCookiePhp == undefined) {
+            refererCookiePhp = document.referrer;
+        }
+
+        return refererCookiePhp;
+    }
+
+    function setCoockieReferer(refererCookie) {
+
+        if (refererCookie.trim() == '') {
+            refererCookie = 'null';
+        }
+
+        var now = new Date();
+        var time = now.getTime();
+        var expireTime = time + 315360000;
+        now.setTime(expireTime);
+        document.cookie = 'referer=' + refererCookie + ';expires=' + now.toGMTString() + ';path=/';
     }
 });
