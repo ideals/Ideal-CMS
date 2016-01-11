5791bdd6acb66cedbfa4694deb5b87e99e7238d0
MORWAL <help7@neox.ru>
2016-01-11 13:50:55 +0500

    FIX: Наследование переменных из родительской области видимости в замыканиях

---
+++ setup/update/3.4/001_fix_min_files.php
@@ -0,0 +1,23 @@
+<?php
+// Обновляем файл "min.gen.php" для css и js
+use Ideal\Core\Config;
+
+$config = Config::getInstance();
+
+$cssMinFalse = DOCUMENT_ROOT . '/css/min.gen.php';
+$jsMinFalse = DOCUMENT_ROOT . '/js/min.gen.php';
+
+$cssMinTrue = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/Ideal/setup/front/css/min.gen.php';
+$jsMinTrue = DOCUMENT_ROOT . '/' . $config->cmsFolder . '/Ideal/setup/front/js/min.gen.php';
+
+$file = file_get_contents($cssMinTrue);
+$file = substr_replace('[[CMS]]', $config->cmsFolder, $file);
+if (!file_put_contents($cssMinFalse, $file)) {
+    echo "не удалось заменить файл $cssMinFalse\n";
+}
+
+$file = file_get_contents($jsMinTrue);
+$file = substr_replace('[[CMS]]', $config->cmsFolder, $file);
+if (!file_put_contents($jsMinFalse, $file)) {
+    echo "не удалось заменить файл $jsMinFalse\n";
+}
