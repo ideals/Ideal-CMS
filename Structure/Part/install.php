3744fe97b49ee508022504c86188cb3e9c8c19ff
Lomov Valentin <help7@neox.ru>
2016-02-11 13:15:46 +0500

    UPD: В процессе установки CMS добавляем текст на главную страницу

--- Structure/Part/install.php
+++ Structure/Part/install.php
@@ -5,6 +5,7 @@ $db = \Ideal\Core\Db::getInstance();
 $cfg = $config->getStructureByName('Ideal_Part');
 
 $table = $config->db['prefix'] . 'ideal_structure_part';
+$tableAddon = $config->db['prefix'] . 'ideal_addon_page';
 
 // Создание таблицы для страниц
 $db->create($table, $cfg['fields']);
@@ -14,6 +15,7 @@ $levels = $cfg['params']['levels'];
 $digits = $cfg['params']['digits'];
 $count = ($levels - 1) * $digits;
 
+// Создаём главную страницу
 $db->insert(
     $table,
     array(
@@ -23,11 +25,22 @@ $db->insert(
         'lvl' => 1,
         'structure' => 'Ideal_Part',
         'template' => 'index.twig',
-        'addon' => '[["1":"Ideal_Page"]]',
+        'addon' => '[["1":"Ideal_Page", "Текст"]]',
         'name' => 'Главная',
         'url' => '/',
         'date_create' => time(),
         'date_mod' => time(),
         'is_active' => 1
+    )
+);
+
+// Создаём текст для главной страницы
+$db->insert(
+    $tableAddon,
+    array(
+        'ID' => 1,
+        'prev_structure' => '1-1',
+        'tab_ID' => '1',
+        'content' => '<p>Это Главная страница Вашего сайта.</p>',
     )
 );
