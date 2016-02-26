6b9a5c178ef0f24a23cc971c9eba961a62f81f55
Lomov Valentin <help7@neox.ru>
2016-02-26 15:39:57 +0500

    FIX: Показ внутренних элементов пункта 'Сервис'

--- Structure/Service/Acl/AjaxController.php
+++ Structure/Service/Acl/AjaxController.php
@@ -10,6 +10,7 @@ namespace Ideal\Structure\Service\Acl;
 
 use \Ideal\Core\Db;
 use \Ideal\Core\Config;
+use Ideal\Structure\Service\Admin\Model as ServiceModel;
 
 /**
  * Реакция на действия со страницы "Права доступа"
@@ -61,7 +62,7 @@ class AjaxController extends \Ideal\Core\AjaxController
                 }
             }
         }
-        return json_encode($permission);
+        return json_encode($permission, JSON_FORCE_OBJECT);
     }
 
     /**
@@ -76,93 +77,103 @@ class AjaxController extends \Ideal\Core\AjaxController
         $structure = $config->getStructureById($structureID);
         $childrenStructure = array();
 
-        // Если идентификатор элемента == 0, то берём из таблицы структуры, чей идентификатор был так же передан
-        // TODO у сервисов нет таблиц, нужно учесть
-        if ($elementID == 0) {
-            $childrenStructure = $config->getStructureById($structureID);
-            $childrenStructure['tableName'] = $config->getTableByName($childrenStructure['structure']);
-        } else { // Если идентификатор элемента отличен от нуля, то сперва нужно узнать тип раздела
-            $structureTable = $config->getTableByName($structure['structure']);
-            $partitionType = $db->select(
-                "SELECT * FROM {$structureTable} WHERE ID = :ID",
-                array(
-                    'ID' => $elementID
-                )
-            );
-            if (!empty($partitionType) && isset($partitionType[0]['structure'])) {
-                $childrenStructure = $config->getStructureByName($partitionType[0]['structure']);
-                $childrenStructure['tableName'] = $config->getTableByName($partitionType[0]['structure']);
-            }
-        }
 
-        // Собираем дочерние элементы
-        if (!empty($childrenStructure)) {
-            $par = array();
-            $whereString = '';
-            if (isset($childrenStructure['fields']['prev_structure'])) {
-                if ($childrenStructure['structure'] == $structure['structure']) {
-                    $par['prev_structure'] = $_POST['prev_structure'];
-                } else {
-                    $par['prev_structure'] = $structureID . '-' . $elementID;
-                }
-                $prev_structure = $par['prev_structure'];
-                $whereString .= ' prev_structure = :prev_structure';
-            }
-
-            // Если идентификатор элемента равен 0, то собираем только первый уровень.
-            if (
-                isset($childrenStructure['fields']['lvl'])
-                && (
-                    $elementID == 0
-                    || $childrenStructure['structure'] != $structure['structure']
-                )
-            ) {
-                if (!empty($whereString)) {
-                    $whereString .= ' AND';
-                }
-                $par['lvl'] = 1;
-                $whereString .= " lvl = :lvl";
-            }
-
-            // Уровень ниже
-            if (
-                isset($childrenStructure['fields']['cid'])
-                && $elementID != 0
-                && $childrenStructure['structure'] == $structure['structure']
-            ) {
-                if (!empty($whereString)) {
-                    $whereString .= ' AND';
-                }
-                $digits = $childrenStructure['params']['digits'];
-                $levels = $childrenStructure['params']['levels'];
-                $cid = $db->select(
-                    "SELECT cid FROM {$childrenStructure['tableName']} WHERE ID = :ID",
-                    array('ID' => $elementID)
+        // Если запрашиваются дочерние элементы пункта отличного от "Сервис"
+        if ($structure['structure'] != 'Ideal_Service') {
+            // Если идентификатор элемента == 0, то берём из таблицы структуры, чей идентификатор был так же передан
+            if ($elementID == 0) {
+                $childrenStructure = $config->getStructureById($structureID);
+                $childrenStructure['tableName'] = $config->getTableByName($childrenStructure['structure']);
+            } else { // Если идентификатор элемента отличен от нуля, то сперва нужно узнать тип раздела
+                $structureTable = $config->getTableByName($structure['structure']);
+                $partitionType = $db->select(
+                    "SELECT * FROM {$structureTable} WHERE ID = :ID",
+                    array(
+                        'ID' => $elementID
+                    )
                 );
-                $cid = str_split($cid[0]['cid'], $digits);
-                $cid = array_filter($cid, function ($v) {
-                    return intval($v);
-                });
-                $cid = implode('', $cid);
-                $par['ID'] = $elementID;
-
-                $cidRegexpString = '^' . $cid . '(.){' . $digits . '}';
-                if (strlen($cidRegexpString) < $digits * $levels) {
-                    $cidRegexpString .= str_repeat('0', $digits);
+                if (!empty($partitionType) && isset($partitionType[0]['structure'])) {
+                    $childrenStructure = $config->getStructureByName($partitionType[0]['structure']);
+                    $childrenStructure['tableName'] = $config->getTableByName($partitionType[0]['structure']);
                 }
-                $whereString .= " cid REGEXP '{$cidRegexpString }' AND ID != :ID";
             }
 
-            // Запрашиваем элементы из базы
-            if (!empty($whereString)) {
-                $whereString = ' WHERE' . $whereString;
-            }
-            $structurePermissions = $db->select(
-                "SELECT * FROM {$childrenStructure['tableName']}{$whereString}",
-                $par
-            );
+            // Собираем дочерние элементы
+            if (!empty($childrenStructure)) {
+                $par = array();
+                $whereString = '';
+                if (isset($childrenStructure['fields']['prev_structure'])) {
+                    if ($childrenStructure['structure'] == $structure['structure']) {
+                        $par['prev_structure'] = $_POST['prev_structure'];
+                    } else {
+                        $par['prev_structure'] = $structureID . '-' . $elementID;
+                    }
+                    $prev_structure = $par['prev_structure'];
+                    $whereString .= ' prev_structure = :prev_structure';
+                }
 
-            // Заполняем полученные данные начальными параметрами доступа
+                // Если идентификатор элемента равен 0, то собираем только первый уровень.
+                if (
+                    isset($childrenStructure['fields']['lvl'])
+                    && (
+                        $elementID == 0
+                        || $childrenStructure['structure'] != $structure['structure']
+                    )
+                ) {
+                    if (!empty($whereString)) {
+                        $whereString .= ' AND';
+                    }
+                    $par['lvl'] = 1;
+                    $whereString .= " lvl = :lvl";
+                }
+
+                // Уровень ниже
+                if (
+                    isset($childrenStructure['fields']['cid'])
+                    && $elementID != 0
+                    && $childrenStructure['structure'] == $structure['structure']
+                ) {
+                    if (!empty($whereString)) {
+                        $whereString .= ' AND';
+                    }
+                    $digits = $childrenStructure['params']['digits'];
+                    $levels = $childrenStructure['params']['levels'];
+                    $cid = $db->select(
+                        "SELECT cid FROM {$childrenStructure['tableName']} WHERE ID = :ID",
+                        array('ID' => $elementID)
+                    );
+                    $cid = str_split($cid[0]['cid'], $digits);
+                    $cid = array_filter($cid, function ($v) {
+                        return intval($v);
+                    });
+                    $cid = implode('', $cid);
+                    $par['ID'] = $elementID;
+
+                    $cidRegexpString = '^' . $cid . '(.){' . $digits . '}';
+                    if (strlen($cidRegexpString) < $digits * $levels) {
+                        $cidRegexpString .= str_repeat('0', $digits);
+                    }
+                    $whereString .= " cid REGEXP '{$cidRegexpString }' AND ID != :ID";
+                }
+
+                // Запрашиваем элементы из базы
+                if (!empty($whereString)) {
+                    $whereString = ' WHERE' . $whereString;
+                }
+                $structurePermissions = $db->select(
+                    "SELECT * FROM {$childrenStructure['tableName']}{$whereString}",
+                    $par
+                );
+            }
+        } elseif (strpos($elementID, '_') === false) {
+            // Если запрашиваются дочерние элементы пункта "Сервис", то собираем их по особенному
+            // Второй уровень вложенности пункта "Сервис" (вкладки), не обслуживается
+            $service = new ServiceModel('');
+            $structurePermissions = $service->getMenu();
+            $childrenStructure['ID'] = $structureID;
+        }
+        // Заполняем полученные данные начальными параметрами доступа
+        if (isset($structurePermissions) && count($structurePermissions) > 0) {
             foreach ($structurePermissions as $structurePermission) {
                 $name = '';
                 if (isset($structurePermission['name'])) {
@@ -182,7 +193,7 @@ class AjaxController extends \Ideal\Core\AjaxController
                         'edit_children' => 1,
                         'delete_children' => 1,
                         'enter_children' => 1,
-                        'prev_structure' => $prev_structure,
+                        'prev_structure' => isset($prev_structure) ? $prev_structure : '',
                     );
                     $aclTable = $config->db['prefix'] . 'ideal_service_acl';
                     $userStructurePermissions = $db->select(
@@ -205,7 +216,7 @@ class AjaxController extends \Ideal\Core\AjaxController
                 }
             }
         }
-        return json_encode($permission);
+        return json_encode($permission, JSON_FORCE_OBJECT);
     }
 
     public function changePermissionAction()
