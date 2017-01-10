fda80fad56b41c3355bcf5977dcd6034ae86ec36
Paul Nikitin <paulnikitin@gmail.com>
2017-01-10 20:26:52 +0300

    FIX: год в копирайте

--- Core/Config.php
+++ Core/Config.php
@@ -2,7 +2,7 @@
 /**
  * Ideal CMS (http://idealcms.ru/)
  * @link      http://github.com/ideals/idealcms репозиторий исходного кода
- * @copyright Copyright (c) 2012-2015 Ideal CMS (http://idealcms.ru)
+ * @copyright Copyright (c) 2012-2017 Ideal CMS (http://idealcms.ru)
  * @license   http://idealcms.ru/license.html LGPL v3
  */
 namespace Ideal\Core;
