c0a2c4b0489ce58be2898da975b7d026ae12c731
Paul Nikitin <paulnikitin@gmail.com>
2017-12-26 00:47:12 +0300

    UPD: CKFinder

--- Library/ckfinder/config.php
+++ Library/ckfinder/config.php
@@ -326,6 +326,16 @@ For more complex configuration options visit our Developer's Guide
 */
 $config['XSendfile'] = false;
 
+/*
+Enables protection in the connector.
+The default CSRF protection mechanism is based on double submit cookies, where
+connector checks if the request contains a valid token that matches the token
+sent in the cookie
+
+https://www.owasp.org/index.php/Cross-Site_Request_Forgery_%28CSRF%29_Prevention_Cheat_Sheet#Double_Submit_Cookies
+*/
+$config['EnableCsrfProtection'] = true;
+
 
 include_once "plugins/imageresize/plugin.php";
 include_once "plugins/fileeditor/plugin.php";
