0bb8d654b31f595c64fd5329a26e540d9e999c72
Paul Nikitin <paulnikitin@gmail.com>
2018-02-19 15:18:23 +0300

    ADD: структура Crm/Inbox

---
+++ Structure/Crm/Inbox/Controller.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Ideal CMS (http://idealcms.ru/)
+ *
+ * @link      http://github.com/ideals/idealcms репозиторий исходного кода
+ * @copyright Copyright (c) 2012-2018 Ideal CMS (http://idealcms.ru)
+ * @license   http://idealcms.ru/license.html LGPL v3
+ */
+
+namespace Ideal\Structure\Crm\Inbox;
+
+use Ideal\Core\Config;
+use Ideal\Core\View;
+
+class Controller
+{
+    /** @var View */
+    private $view;
+
+    public function run()
+    {
+        $this->templateInit();
+        return $this->view->render();
+    }
+
+    /**
+     * Инициализация twig-шаблона
+     *
+     * @param string $tplName Название файла шаблона (с путём к нему), если не задан - будет index.twig
+     */
+    public function templateInit($tplName = '')
+    {
+        // Определение названия модуля из названия класса контроллера
+        $parts = explode('\\', get_class($this));
+        $moduleName = $parts[0];
+        $moduleName = ($moduleName === 'Ideal') ? '' : $moduleName . '/';
+        $structureName = $parts[2];
+        $crmName = $parts[3];
+
+        // Инициализация шаблона страницы
+        if ($tplName == '') {
+            $tplName = $moduleName . 'Structure/' . $structureName . '/'. $crmName . '/index.twig';
+        }
+        $tplRoot = dirname(stream_resolve_include_path($tplName));
+        $tplName = basename($tplName);
+
+        if ($tplRoot == '') {
+            // Если в структуре нет файла шаблона, пытаемся его найти в модуле
+            $tplName = $moduleName . 'Structure/' . $structureName . '/' . $crmName . '/index.twig';
+            if (!stream_resolve_include_path($tplName)) {
+                echo 'Нет файла шаблона ' . $tplName;
+                exit;
+            }
+            $tplRoot = dirname(stream_resolve_include_path($tplName));
+            $tplName = basename($tplName);
+        }
+
+        // Инициализируем Twig-шаблонизатор
+        $config = Config::getInstance();
+        $this->view = new View(array($tplRoot), $config->cache['templateAdmin']);
+        $this->view->loadTemplate($tplName);
+    }
+}
---
+++ Structure/Crm/Inbox/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Ideal CMS (http://idealcms.ru/)
+ *
+ * @link      http://github.com/ideals/idealcms репозиторий исходного кода
+ * @copyright Copyright (c) 2012-2018 Ideal CMS (http://idealcms.ru)
+ * @license   http://idealcms.ru/license.html LGPL v3
+ */
+
+return array(
+    'name' => 'Входящие',
+    'pos' => 10,
+    'info' => '',
+    'structure' => 'Ideal_Crm',
+    'ID' => 'Ideal_Inbox'
+);
