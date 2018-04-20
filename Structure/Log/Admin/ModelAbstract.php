b03d81417be3ae5e1b28c5f3bf6e10d68a7827da
Валентин Ломов <help7@neox.ru>
2018-04-20 18:04:44 +0500

    FIX: в справочние логов на странице списка отображается email администратора вместо идентификатора

--- Structure/Log/Admin/ModelAbstract.php
+++ Structure/Log/Admin/ModelAbstract.php
@@ -9,7 +9,26 @@
 
 namespace Ideal\Structure\Log\Admin;
 
+use Ideal\Core\Config;
+use Ideal\Core\Db;
+
 class ModelAbstract extends \Ideal\Structure\Roster\Admin\ModelAbstract
 {
+    public function getList($page = null)
+    {
+        $logList = parent::getList($page);
 
+        $db = Db::getInstance();
+        $config = Config::getInstance();
+        $table = $config->db['prefix'] . 'ideal_structure_user';
+        $sql = 'SELECT ID, email FROM ' . $table;
+        $arr = $db->select($sql);
+        foreach ($arr as $item) {
+            $arr[$item['ID']] = $item['email'];
+        }
+        foreach ($logList as &$logListItem) {
+            $logListItem['user_id'] = $arr[$logListItem['user_id']];
+        }
+        return $logList;
+    }
 }
