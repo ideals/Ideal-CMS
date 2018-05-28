75e84cd9efaa136f7f76669ffc73fadbd0ddab67
Валентин Ломов <help7@neox.ru>
2018-05-28 18:01:43 +0500

    FIX: подключение структуры CRM

--- setup/update/4.5/new_003_change_crm_include.php
+++ setup/update/4.5/new_003_change_crm_include.php
@@ -14,16 +14,35 @@ if ($crmStructure) {
     if (!file_exists($fileName)) {
         throw new \Exception('Файл не найден: ' . $fileName);
     }
+    $file = file_get_contents($fileName);
 
-    $configFile = include($fileName);
+    // Локализум место подключения структуры CRM
+    $pos = strrpos($file, 'Ideal_Crm');
+    $fileTmp = substr($file, 0, $pos);
+    $fileTmp2 = substr($file, $pos);
 
-    // Ищем структуру Ideal_Crm
-    foreach ($configFile['structures'] as $key => $structure) {
-        if ($structure['structure'] == 'Ideal_Crm') {
-            $configFile['structures'][$key]['isShow'] = 1;
-            $configFile['structures'][$key]['hasTable'] = false;
-        }
-    }
+    // Позиция начала описания включения структуры CRM
+    $posCrmStart = strrpos($fileTmp, 'array(');
 
-    file_put_contents($fileName, "<?php \n return " . var_export($configFile, 1) . ";\n");
+    // Позиция окончания описания включения структуры CRM
+    $posCrmEnd = strpos($fileTmp2, 'array(');
+
+    // Вырезаем из основного файла подключения структуры CRM
+    $file = substr($fileTmp, 0, $posCrmStart) . substr($fileTmp2, $posCrmEnd);
+
+    // Возвращаем изменённое подключение структуры CRM
+    $add = <<<ADD
+        // Подключаем структуру CRM
+        array(
+            'ID' => {$crmStructure['ID']},
+            'structure' => 'Ideal_Crm',
+            'name' => 'CRM',
+            'isShow' => 1,
+            'hasTable' => false,
+        ),
+ADD;
+    $pos = strrpos($file, ',');
+    $file = substr($file, 0, $pos + 1) . $add . substr($file, $pos + 1);
+    file_put_contents($fileName, $file);
+    $config->loadSettings();
 }
