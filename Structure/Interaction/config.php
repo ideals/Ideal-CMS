790b1fd9322e404e117cc57c09f86fe54b9cdf57
Валентин Ломов <help7@neox.ru>
2018-08-02 13:29:58 +0500

    ADD: скилет структуры 'Взаимодействия' общей для всех возможных взаимодействий

---
+++ Structure/Interaction/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Ideal CMS (http://idealcms.ru/)
+ *
+ * @link      http://github.com/ideals/idealcms репозиторий исходного кода
+ * @copyright Copyright (c) 2012-2018 Ideal CMS (http://idealcms.ru)
+ * @license   http://idealcms.ru/license.html LGPL v3
+ */
+
+// Взаимодействие
+return array(
+    'params' => array(
+        'field_sort' => 'date_create DESC',
+        'field_list' => array('date_create', 'type'),
+        'elements_cms' => PHP_INT_MAX
+    ),
+);
