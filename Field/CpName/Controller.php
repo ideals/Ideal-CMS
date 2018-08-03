b95f55421530eeb0ba15d5732154dc1e6de97d16
Валентин Ломов <help7@neox.ru>
2018-08-03 16:23:13 +0500

    FIX: описания классов полей используемых для отображения информации по контактным лицам в списке лидов

--- Field/CpName/Controller.php
+++ Field/CpName/Controller.php
@@ -16,11 +16,12 @@ use Ideal\Field\AbstractController;
  *
  * Отображается в виде скрытого поля ввода <input type="hidden" />
  *
+ * Используется в структуре "Ideal_Lead" для отображения имён из списка Контактных лиц отнесённых к лиду
+ *
  * Пример объявления в конфигурационном файле структуры:
- *     'date_create' => array(
- *         'label' => 'ID родительских структур',
- *         'sql'   => 'char(15)',
- *         'type'  => 'Ideal_Hidden'
+ *     'cpName' => array(
+ *         'label' => 'Список имён контактных лиц лида',
+ *         'type' => 'Ideal_CpName'
  *     ),
  */
 class Controller extends AbstractController
